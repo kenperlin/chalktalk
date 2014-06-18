f8ea699c3971929c857057675df35697544638d2
Ken Perlin <KenPerlin@Kens-MacBook-Air.local>
2014-06-18 09:38:00 +0100

    Made the graphics clearer for Grid (which is for explaining noise algorithm). Added 4th state (triggered by drag down) to Grid: removes all the scaffolding.

--- june26.js
+++ june26.js
@@ -352,31 +352,44 @@ function Grid() {
       var f = 2/3;
       m.save();
          m.scale(this.size / 400);
-         mCurve([[-1,0], [1, 0]]);
-         mCurve([[ 0,1], [0, -1]]);
+	 if (this.gridMode != 3) {
+            mCurve([[-1,0], [1, 0]]);
+            mCurve([[ 0,1], [0, -1]]);
+         }
 	 this.afterSketch(function(S) {
-            mCurve([[-1, f], [1, f]]);
-            mCurve([[-1,-f], [1,-f]]);
-            mCurve([[-f,1], [-f,-1]]);
-            mCurve([[ f,1], [ f,-1]]);
+	    if (S.gridMode != 3) {
+               mCurve([[-1, f], [1, f]]);
+               mCurve([[-1,-f], [1,-f]]);
+               mCurve([[-f,1], [-f,-1]]);
+               mCurve([[ f,1], [ f,-1]]);
+            }
+	    var uColor = 'rgb(255,64,64)';
+	    var vColor = 'rgb(64,255,64)';
 	    switch (S.gridMode) {
+            case 3:
             case 2:
 	       var d = 1/20;
-	       color('yellow');
+	       var e = d/2;
 	       lineWidth(0.5);
-	       for (var u = -1 ; u <= 1 ; u += d)
-	       for (var v = -1 ; v <= 1 ; v += d) {
-	          var t0 = noise2(u, v);
-	          var tu = noise2(u+d, v);
-	          var tv = noise2(u, v+d);
-		  if (u < 1)
+	       for (var u = -1 ; u <= 1 + d/2 ; u += d)
+	       for (var v = -1 ; v <= 1 + d/2 ; v += d) {
+	          var t0 = noise2(u  , v  )*f;
+	          var tu = noise2(u+d, v  )*f;
+	          var tv = noise2(u  , v+d)*f;
+		  if (u < 1) {
+		     color(uColor);
 	             mCurve([[u*f,v*f,t0] , [(u+d)*f,v*f,tu]]);
-		  if (v < 1)
+                  }
+		  if (v < 1) {
+		     color(vColor);
 	             mCurve([[u*f,v*f,t0] , [u*f,(v+d)*f,tv]]);
+                  }
                }
+	       if (S.gridMode == 3)
+	          break;
 	    case 1:
-	       lineWidth(1);
-	       color('rgb(64,255,64)');
+	       lineWidth(4);
+	       color(vColor);
 	       for (var u = -1 ; u <= 1 ; u += 1)
 	       for (var v = -1 ; v <= 1 ; v += 1) {
 	          var t0 = noise2(u, v    );
@@ -385,8 +398,8 @@ function Grid() {
 	          mCurve([[u*f,v*f-.1,-s] , [u*f,v*f+.1,s]]);
                }
 	    case 0:
-	       lineWidth(1);
-	       color('red');
+	       lineWidth(4);
+	       color(uColor);
 	       for (var u = -1 ; u <= 1 ; u += 1)
 	       for (var v = -1 ; v <= 1 ; v += 1) {
 	          var t0 = noise2(u    , v);
