190298449a278ca30f31bdbb6698b0e09d32c8e2
Ken Perlin <KenPerlin@172-26-15-6.dynapool.nyu.edu>
2014-06-26 13:13:08 -0400

    Implemented first part of Lattice.

--- june26.js
+++ june26.js
@@ -448,6 +448,47 @@ function sliced() {
 }
 
 
+function Lattice() {
+   this.labels = "lattice".split(' ');
+   this.is3D = true;
+   this.showLattice = false;
+   this.showCube = false;
+   this.onClick = function() {
+      if (this.showLattice)
+         this.showCube = true;
+      else
+         this.showLattice = true;
+   }
+   this.render = function(elapsed) {
+      m.save();
+         m.scale(this.size / 400);
+         m.rotateY(-.2);
+         m.rotateX( .2);
+	 if (this.showLattice)
+	    lineWidth(1);
+         mCurve([[-1, 0, 0], [ 1, 0, 0]]);
+         mCurve([[ 0,-1, 0], [ 0, 1, 0]]);
+         mCurve([[ 0, 0,-1], [ 0, 0, 1]]);
+	 if (this.showLattice) {
+	    m.scale(.5);
+	    for (var x = -2 ; x <= 2 ; x++)
+	    for (var y = -2 ; y <= 2 ; y++)
+	    for (var z = -2 ; z <= 2 ; z++) {
+	       mCurve([[x-.03,y,z],[x+.03,y,z]]);
+	       mCurve([[x,y-.03,z],[x,y+.03,z]]);
+	    }
+	    if (this.showCube) {
+	       color('pink');
+	       mLine([-2, 2, 2],[-1, 2, 2]);
+	       mLine([-2, 2, 2],[-2, 1, 2]);
+	       mLine([-2, 2, 2],[-2, 2, 1]);
+	    }
+	 }
+      m.restore();
+   }
+}
+Lattice.prototype = new Sketch;
+
 function Grid() {
    this.labels = "grid".split(' ');
    this.gridMode = -1;
