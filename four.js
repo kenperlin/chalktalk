b2562957a5ae8bdb89788201a842220f18dec8a3
Ken Perlin <KenPerlin@Kens-MacBook-Pro.local>
2014-07-01 14:03:37 -0700

    Added non-sketch related WebGL stuff from g.js to four.js.

--- four.js
+++ four.js
@@ -535,4 +535,142 @@
       renderer.camera.position.set(0,0,10);
    }
 
+function gl() { return renderer.context; }
+function isValidVertexShader  (string) { return isValidShader(gl().VERTEX_SHADER  , string); }
+function isValidFragmentShader(string) { return isValidShader(gl().FRAGMENT_SHADER, string); }
+function isValidShader(type, string) {
+   string = "precision highp float;\n" + string;
+   var shader = gl().createShader(type);
+   gl().shaderSource(shader, string);
+   gl().compileShader(shader);
+   return gl().getShaderParameter(shader, gl().COMPILE_STATUS);
+};
+
+function addUniforms(material, string) {
+
+   // PARSE THE FRAGMENT SHADER CODE TO FIND CUSTOM UNIFORMS:
+
+   var typeInfo = "float f 0 vec3 v2 [0,0] vec3 v3 [0,0,0]".split(' ');
+   var declarations = string.substring(0, string.indexOf("void main")).split(";");
+   for (var i = 0 ; i < declarations.length ; i++) {
+      var declaration = declarations[i].trim();
+      if (declaration.length > 0) {
+
+         var words = declaration.split(" ");
+         if (words[0] == 'uniform') {
+            var type = words[1];
+            var name = words[2];
+            for (var n = 0 ; n < typeInfo.length ; n += 3)
+               if (type == typeInfo[n]) {
+                  material.uniforms[name] = { type: typeInfo[n+1], value: typeInfo[n+2] };
+                  break;
+               }
+         }
+      }
+   }
+}
+
+function formFragmentShader(string) {
+
+   // PREPEND THE HEADER OF PREDEFINED THINGS:
+
+   return fragmentShaderHeader.concat(string);
+}
+
+function shaderMaterial(vertexShader, fragmentShaderString) {
+   var material = new THREE.ShaderMaterial({
+      uniforms: {},
+      vertexShader: vertexShader,
+   });
+
+   var u = "alpha mx my selectedIndex time".split(' ');
+   for (var i = 0 ; i < u.length ; i++)
+      material.uniforms[u[i]] = { type: "f", value: (u[i]=="alpha" ? 1 : 0) };
+
+   addUniforms(material, fragmentShaderString);
+   material.fragmentShader = formFragmentShader(fragmentShaderString);
+
+   return material;
+}
+
+// THIS VERTEX SHADER WILL SUFFICE FOR MOST SHADER PLANES:
+
+var defaultVertexShader = ["\
+   varying float x;\
+   varying float y;\
+   varying vec3 vPosition;\
+   varying vec3 vNormal;\
+   void main() {\
+      x = 2. * uv.x - 1.;\
+      y = 2. * uv.y - 1.;\
+      vNormal = (modelViewMatrix * vec4(normal, 0.)).xyz;\
+      vPosition = position*.03;\
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.);\
+   }\
+"].join("\n");
+
+// DEFINES FRAGMENT SHADER FUNCTIONS noise() and turbulence() AND VARS x, y, time and alpha.
+
+var fragmentShaderHeader = ["\
+   vec3 mod289(vec3 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }\
+   vec4 mod289(vec4 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }\
+   vec4 permute(vec4 x) { return mod289(((x*34.0)+1.0)*x); }\
+   vec4 taylorInvSqrt(vec4 r) { return 1.79284291400159 - 0.85373472095314 * r; }\
+   vec3 fade(vec3 t) { return t*t*t*(t*(t*6.0-15.0)+10.0); }\
+   float noise(vec3 P) {\
+      vec3 i0 = mod289(floor(P)), i1 = mod289(i0 + vec3(1.0));\
+      vec3 f0 = fract(P), f1 = f0 - vec3(1.0), f = fade(f0);\
+      vec4 ix = vec4(i0.x, i1.x, i0.x, i1.x), iy = vec4(i0.yy, i1.yy);\
+      vec4 iz0 = i0.zzzz, iz1 = i1.zzzz;\
+      vec4 ixy = permute(permute(ix) + iy), ixy0 = permute(ixy + iz0), ixy1 = permute(ixy + iz1);\
+      vec4 gx0 = ixy0 * (1.0 / 7.0), gy0 = fract(floor(gx0) * (1.0 / 7.0)) - 0.5;\
+      vec4 gx1 = ixy1 * (1.0 / 7.0), gy1 = fract(floor(gx1) * (1.0 / 7.0)) - 0.5;\
+      gx0 = fract(gx0); gx1 = fract(gx1);\
+      vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0), sz0 = step(gz0, vec4(0.0));\
+      vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1), sz1 = step(gz1, vec4(0.0));\
+      gx0 -= sz0 * (step(0.0, gx0) - 0.5); gy0 -= sz0 * (step(0.0, gy0) - 0.5);\
+      gx1 -= sz1 * (step(0.0, gx1) - 0.5); gy1 -= sz1 * (step(0.0, gy1) - 0.5);\
+      vec3 g0 = vec3(gx0.x,gy0.x,gz0.x), g1 = vec3(gx0.y,gy0.y,gz0.y),\
+           g2 = vec3(gx0.z,gy0.z,gz0.z), g3 = vec3(gx0.w,gy0.w,gz0.w),\
+           g4 = vec3(gx1.x,gy1.x,gz1.x), g5 = vec3(gx1.y,gy1.y,gz1.y),\
+           g6 = vec3(gx1.z,gy1.z,gz1.z), g7 = vec3(gx1.w,gy1.w,gz1.w);\
+      vec4 norm0 = taylorInvSqrt(vec4(dot(g0,g0), dot(g2,g2), dot(g1,g1), dot(g3,g3)));\
+      vec4 norm1 = taylorInvSqrt(vec4(dot(g4,g4), dot(g6,g6), dot(g5,g5), dot(g7,g7)));\
+      g0 *= norm0.x; g2 *= norm0.y; g1 *= norm0.z; g3 *= norm0.w;\
+      g4 *= norm1.x; g6 *= norm1.y; g5 *= norm1.z; g7 *= norm1.w;\
+      vec4 nz = mix(vec4(dot(g0, vec3(f0.x, f0.y, f0.z)), dot(g1, vec3(f1.x, f0.y, f0.z)),\
+                         dot(g2, vec3(f0.x, f1.y, f0.z)), dot(g3, vec3(f1.x, f1.y, f0.z))),\
+                    vec4(dot(g4, vec3(f0.x, f0.y, f1.z)), dot(g5, vec3(f1.x, f0.y, f1.z)),\
+                         dot(g6, vec3(f0.x, f1.y, f1.z)), dot(g7, vec3(f1.x, f1.y, f1.z))), f.z);\
+      return 2.2 * mix(mix(nz.x,nz.z,f.y), mix(nz.y,nz.w,f.y), f.x);\
+   }\
+   float noise(vec2 P) { return noise(vec3(P, 0.0)); }\
+   float fractal(vec3 P) {\
+      float f = 0., s = 1.;\
+      for (int i = 0 ; i < 9 ; i++) {\
+         f += noise(s * P) / s;\
+         s *= 2.;\
+         P = vec3(.866 * P.x + .5 * P.z, P.y + 100., -.5 * P.x + .866 * P.z);\
+      }\
+      return f;\
+   }\
+   float turbulence(vec3 P) {\
+      float f = 0., s = 1.;\
+      for (int i = 0 ; i < 9 ; i++) {\
+         f += abs(noise(s * P)) / s;\
+         s *= 2.;\
+         P = vec3(.866 * P.x + .5 * P.z, P.y + 100., -.5 * P.x + .866 * P.z);\
+      }\
+      return f;\
+   }\
+   varying vec3 vNormal;\
+   varying vec3 vPosition;\
+   varying float x;\
+   varying float y;\
+   uniform float mx;\
+   uniform float my;\
+   uniform float time;\
+   uniform float selectedIndex;\
+   uniform float alpha;\
+"].join("\n");
 
