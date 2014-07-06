2d6d8532e370f9782d3b6b474f1b237dfcfd6d92
David Lobser <david@davids-mbp.home>
2014-07-06 19:12:44 -0400

    removed arbrevolve

--- june26-sketch-pages.js
+++ june26-sketch-pages.js
@@ -1,4 +1,4 @@
-var sketchTypes = ( "Bird Control Diagram Graph Logic Func Physics Shape3D IO Marker Grid Lattice PVase ArbRevolve ArbRevolveHandle Noises MothAndCandle" ).split(' ');
+var sketchTypes = ( "Bird Control Diagram Graph Logic Func Physics Shape3D IO Marker Grid Lattice PVase ArbRevolveHandle Noises MothAndCandle" ).split(' ');
 
 function blankPage() {
    return {
