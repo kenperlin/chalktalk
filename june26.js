b8e7284a1b1861c223137453ae5265802f9acaf4
Ken Perlin <KenPerlin@Kens-MacBook-Pro.local>
2014-07-08 06:43:46 -0700

    Created subtle red glowing edges for flameFragmentShader objects.

--- june26.js
+++ june26.js
@@ -370,9 +370,9 @@ var flameFragmentShader = ["\
       vec3 p = 20.*vPosition;\
       float nx = .5 * noise(.1*p);\
       float ny = .5 * noise(.1*p + vec3(100., 0., 0.));\
-      float s = .5 * p.z + turbulence(vec3(p.x+nx, p.y+ny, p.z+.3*time));\
+      float s = .25 * p.z + turbulence(vec3(p.x+nx, p.y+ny, p.z+.3*time));\
       float ss = s * s;\
-      vec3 color = 2. * s * vec3(1., ss, ss*ss) * vNormal.z;\
+      vec3 color = mix(vec3(.35,0.,0.), s * vec3(1.,ss,ss*ss), min(1., 2.*vNormal.z));\
       gl_FragColor = vec4(color, alpha);\
    }\
 "].join("\n");
