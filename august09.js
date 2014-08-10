9d117469b7c593dd663db4819c6532745739e185
Ken Perlin <kenperlin@gmail.com>
2014-08-10 07:35:54 -0700

    Tuned the behavior of the rocket ship.

--- august09.js
+++ august09.js
@@ -1,31 +1,35 @@
 
    function Rocket() {
       this.labels = "rocket".split(' ');
-      this.accel = 0;
+      this.swipeTime = 0;
       this.velocity = 0;
       this.altitude = 0;
       this.onSwipe = function(dx,dy) {
          var n = pieMenuIndex(dx, dy, 8);
 	 switch (n) {
 	 case 2:
-	    this.accel = .25;
+	    this.swipeTime = time + 0.5;
 	    break;
 	 }
       }
       this.render = function(elapsed) {
-         this.velocity += this.accel * elapsed;
-         this.altitude += this.velocity;
+         if (this.swipeTime != 0 && time > this.swipeTime) {
+	    console.log("AHA");
+            this.velocity += 0.4 * elapsed;
+            this.altitude += this.velocity;
+         }
+
          m.save();
 
 	 // ANIMATE ROCKET TRAJECTORY
 
-         var t = max(0, this.altitude - 0.5);
+         var t = this.altitude;
 	 m.rotateZ(-0.02 * t);
 	 m.translate(0, t, 0);
 
 	 // DRAW FUSILAGE
 
-	 mCurve(createSpline([[0,1],[-.4,0],[-.25,-1]]));
+	 mCurve(createSpline([[-.25,-1],[-.4,0],[0,1]]));
 	 mCurve(createSpline([[0,1],[ .4,0],[ .25,-1]]));
 
 	 // DRAW WINGS
@@ -36,11 +40,10 @@
 	 // THRUSTER FLAMES
 
 	 this.afterSketch(function(elapsed) {
-	 if (this.accel > 0) {
-	    var n0 = noise(10*time,0,100) * .5;
-	    var n1 = noise(10*time,0,200) * .5;
-	    var n2 = noise(10*time,0,300) * .5;
-	    mCurve([[-.2,-1.1],[-.3,-1.4+n0],[-.05,-1.15],[0,-1.5+n1],[ .05,-1.15],[ .3,-1.4+n2],[ .2,-1.1]]);
+	 if (this.swipeTime != 0) {
+	    mCurve([[-.20, -1.10], [-.3, -1.4 + .7*noise2(10*time, 100)], 
+	            [-.08, -1.15], [  0, -1.6 + .7*noise2(10*time, 200)], 
+		    [ .08, -1.15], [ .3, -1.4 + .7*noise2(10*time, 300)], [ .2, -1.1]]);
 	 }});
 
          m.restore();
