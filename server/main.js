8d7d1877c3d4615b6612dc7ca9c0b42ae1b457a3
Evan Moore <2emoore4@gmail.com>
2014-08-25 21:00:08 -0400

    some server changes. able to receive uploaded files, then save them.

--- server/main.js
+++ server/main.js
@@ -1,45 +1,38 @@
+var bodyParser = require("body-parser");
+var express = require("express");
+var formidable = require("formidable");
 var fs = require("fs");
-var http = require("http");
 var path = require("path");
-var url = require("url");
+
+var app = express();
 var port = process.argv[2] || 8888;
 
-http.createServer(function (request, response) {
-   var uri = url.parse(request.url).pathname;
-   var filename = path.join(process.cwd(), uri);
+// serve static files from main directory
+app.use(express.static("./"));
 
-   var contentTypesByExtension = {
-      ".html": "text/html",
-      ".css": "text/css",
-      ".js": "text/javascript"
-   };
+// handle uploaded files
+app.use(bodyParser({defer: true}));
+app.route("/upload").post(function(req, res, next) {
+   var form = formidable.IncomingForm();
+   form.uploadDir = "./sketches";
+   form.keepExtensions = true;
 
-   fs.exists(filename, function(exists) {
-      if (!exists) {
-         response.writeHead(404, {"Content-Type": "text/plain"});
-         response.write("404 Not Found\n");
-         response.end();
-         return;
-      }
+   form.parse(req, function(err, fields, files) {
+      res.writeHead(200, {"content-type": "text/plain"});
+      res.write('received upload:\n\n');
 
-      if (fs.statSync(filename).isDirectory()) filename += '/index.html';
-
-      fs.readFile(filename, "binary", function(err, file) {
+      fs.writeFile(form.uploadDir + "/" + fields.sketchName + ".js", fields.sketchContent, function(err) {
          if (err) {
-            response.writeHead(500, {"Content-Type": "text/plain"});
-            response.write(err + "\n");
-            response.end();
-            return;
+            console.log(err);
+         } else {
+            console.log("file written");
          }
-
-         var headers = {};
-         var contentType = contentTypesByExtension[path.extname(filename)];
-         if (contentType) headers["Content-Type"] = contentType;
-         response.writeHead(200, headers);
-         response.write(file, "binary");
-         response.end();
       });
-   });
-}).listen(parseInt(port, 10));
 
-console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
+      res.end();
+   });
+});
+
+var server = app.listen(parseInt(port, 10), function() {
+   console.log("Listening on port %d", server.address().port);
+});
