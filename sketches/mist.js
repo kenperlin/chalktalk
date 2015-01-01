1d6a495ca92366973d81b6eb27750ac3b69f0a53
kenperlin <kenperlin@gmail.com>
2015-01-01 09:17:12 -0500

    Slight clean-up of drawing logic in sketches/mist.js.

--- sketches/mist.js
+++ sketches/mist.js
@@ -18,34 +18,28 @@ function Mist() {
       var a =  0.3;
       var b = -0.25;
       var e =  0.23;
+      var w = .95;
 
       _g.lineCap = 'square';
       var lw = this.mScale(.3);
       lineWidth(lw);
 
-      var w = .95;
       var f = e * 4 / 5;
-      var path = [];
-      path.push([-w    ,-h]);
-      path.push([-w    , h - f]);
-      path.push([-w+2*f, h + f]);
-      path.push([  -2*f, h + f]);
-      path.push([0     , h - f]);
-      path.push([0     ,-h]);
-      mCurve(createPipe(path));
-
-      var path = [];
-      path.push([0    ,-h]);
-      path.push([0    , h - f]);
-      path.push([  2*f, h + f]);
-      path.push([w-2*f, h + f]);
-      path.push([w    , h - f]);
-      path.push([w    ,-h]);
-      mCurve(createPipe(path));
+      for (var i = 0 ; i < 2 ; i++) {
+         var path = [];
+         path.push([-w    ,-h]);
+         path.push([-w    , h - f]);
+         path.push([-w+2*f, h + f]);
+         path.push([  -2*f, h + f]);
+         path.push([0     , h - f]);
+         path.push([0     ,-h]);
+         mCurve(createPipe(path));
+         m.translate(w,0,0);
+      }
 
       this.afterSketch(function() {
 
-      m.translate(2.55,0,0);
+      m.translate(0.65,0,0);
 
       mFillCurve(createRoundRect(-1-e,h-e*.55,2*e,2*e,e/2));
 
