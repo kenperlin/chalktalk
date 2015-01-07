7b433c23d429aa9dda266d75ec5bf4cad2780439
kenperlin <kenperlin@gmail.com>
2015-01-06 20:38:47 -0500

    Updated airfoil.

--- sketches/airfoil.js
+++ sketches/airfoil.js
@@ -6,39 +6,27 @@ function Airfoil() {
          mCurve( [[-1,0],[-.8,.2],[1,0]] );
          mCurve( [[-1,0],[1,0]] );
       });
+      this.afterSketch(function() {
+         if (this.mesh != null && this.airfoil === undefined) {
+	    var airfoil = this.airfoil = this.mesh.addNode();
+            airfoil.addCylinder(16).getMatrix().rotateZ(PI/2).rotateX(-PI/2).scale(.1,12,1);
+            var shape = airfoil.children[0].geometry;
+            var vertices = shape.vertices;
+            for (var i = 0 ; i < vertices.length ; i++) {
+               var v = vertices[i];
+	       v.x -= .6 * (v.z + 1) * (v.z - 1);
+	       v.x += v.z * (v.x + .5);
+            }
+            shape.computeCentroids();
+            shape.computeVertexNormals();
+            airfoil.setMaterial(new THREE.MeshPhongMaterial());
+            airfoil.material.color = airfoil.material.emissive = new THREE.Color(0x404040);
+	 }
+      });
    }
 
    this.createMesh = function() {
-      var airfoil = new THREE.Mesh();
-      airfoil.addCylinder(16).getMatrix().rotateZ(PI/2).rotateX(-PI/2).scale(.1,2,1);
-      var shape = airfoil.children[0].geometry;
-      var vertices = shape.vertices;
-      for (var i = 0 ; i < vertices.length ; i++) {
-         var v = vertices[i];
-	 v.x -= .6 * (v.z + 1) * (v.z - 1);
-	 v.x += v.z * (v.x + .5);
-      }
-      shape.computeCentroids();
-      shape.computeVertexNormals();
-/*
-      var normals = shape.attributes["normal"].array;
-      for (var i = 0 ; i < vertices.length ; i++) {
-         var v = vertices[i];
-	 if (v.y == -1) {
-	    normals[3 * i    ] =  0;
-	    normals[3 * i + 1] = -1;
-	    normals[3 * i + 2] =  0;
-	 }
-	 else if (v.y == 1) {
-	    normals[3 * i    ] =  0;
-	    normals[3 * i + 1] =  1;
-	    normals[3 * i + 2] =  0;
-	 }
-      }
-*/
-      airfoil.setMaterial(new THREE.MeshPhongMaterial());
-      airfoil.material.color = airfoil.material.emissive = new THREE.Color(0x202020);
-      return airfoil;
+      return new THREE.Mesh();
    }
 }
 Airfoil.prototype = new Sketch;
