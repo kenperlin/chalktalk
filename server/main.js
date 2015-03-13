3f8df172c6eeb5c7785b862d850423def8fb15df
Evan Moore <2emoore4@gmail.com>
2015-03-12 21:57:23 -0400

    removing dead code

--- server/main.js
+++ server/main.js
@@ -2,12 +2,8 @@ var bodyParser = require("body-parser");
 var express = require("express");
 var formidable = require("formidable");
 var fs = require("fs");
-//var git = require("nodegit");
 var path = require("path");
 var readline = require("readline-sync");
-
-//var gitUser = readline.question("github username: ");
-//var gitPass = readline.question("github password for " + gitUser + ":", {noEchoBack: true});
 
 var app = express();
 var port = process.argv[2] || 11235;
@@ -29,7 +25,7 @@ app.route("/upload").post(function(req, res, next) {
       var filename = fields.sketchName;
       var suffix = ".js";
       if (filename.indexOf(suffix, filename.length - suffix.length) == -1)
-         filename += suffix; 
+         filename += suffix;
 
       fs.writeFile(form.uploadDir + "/" + filename, fields.sketchContent, function(err) {
          if (err) {
@@ -53,7 +49,7 @@ app.route("/set").post(function(req, res, next) {
 
       var suffix = ".json";
       if (key.indexOf(suffix, key.length - suffix.length) == -1)
-         key += suffix; 
+         key += suffix;
 
       fs.writeFile(key, fields.value, function(err) {
          if (err) {
@@ -64,61 +60,6 @@ app.route("/set").post(function(req, res, next) {
       });
 
       res.end();
-   });
-});
-
-/*
-// open git repo
-var repository;
-git.Repo.open(".git", function(err, repo) {
-   if (err) throw err;
-   repository = repo;
-});
-*/
-
-// handle commit requests
-app.route("/commit").post(function(req, res, next) {
-   var form = formidable.IncomingForm();
-
-   form.parse(req, function(err, fields, files) {
-      res.writeHead(200, {"content-type": "text/plain"});
-      res.write("received commit request");
-
-      var filename = fields.sketchName;
-      var suffix = ".js";
-      if (filename.indexOf(suffix, filename.length - suffix.length) == -1)
-         filename += suffix; 
-/*
-      // I know this is confusing but I'll fix it 
-      repository.openIndex(function(err, index) {
-         if (err) throw err;
-         index.read(function(err) {
-            if (err) throw err;   
-            index.addByPath("sketches/" + filename, function(err) {
-               if (err) throw err;
-               index.write(function(err) {
-                  if (err) throw err;
-                  index.writeTree(function(err, oid) {
-                     if (err) throw err;
-                     git.Reference.oidForName(repository, 'HEAD', function(err, head) {
-                        if (err) throw err; 
-                        repository.getCommit(head, function(err, parent) {
-                           if (err) throw err;
-                           var author = git.Signature.now("Evan Moore", "2emoore4@gmail.com");
-                           var committer = git.Signature.now("Evan Moore", "2emoore4@gmail.com");
-
-                           repository.createCommit('HEAD', author, committer, 'new sketch from web editor', oid, [parent], function(err, commitId) {
-                              console.log("New commit: " + commitId.sha());
-                           });
-                        });
-                     });
-                  });
-               });
-            });
-         });
-
-      });
-*/
    });
 });
 
@@ -134,7 +75,7 @@ app.route("/ls_sketches").get(function(req, res) {
          for (var i = 0; i < files.length; i++) {
             res.write(files[i] + "\n");
          }
-      } 
+      }
       res.end();
    });
 });
