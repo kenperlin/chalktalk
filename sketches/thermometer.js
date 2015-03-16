97d8276cd8baa4208a8562862ec3362f2503290b
kenperlin <kenperlin@gmail.com>
2015-03-16 04:42:06 -0400

    Added input option to thermometer.

--- sketches/thermometer.js
+++ sketches/thermometer.js
@@ -1,13 +1,13 @@
 function() {
    var cursor = newVec3();
-   this.label = "thermomet";
+   this.label = "thermometer";
    this.onPress = function(point) { cursor.copy(point); }
    this.onDrag  = function(point) { cursor.copy(point); }
    this.render = function(elapsed) {
       mCurve([[-.1,-.6]].concat(arc(0,.9,.1,PI,0)).concat([[.1,-.6]]));
       mCurve(arc(0,-.78,.2,PI*2/3,PI*7/3));
       this.afterSketch(function() {
-         var value = max(0, min(1, (cursor.y + .55) / 1.45));
+	 var value = max(0, min(1, this.getInValue(0, (cursor.y + .55) / 1.45)));
          mFillRect([-.05,-.8 ], [.05, 1.45 * value - .55]);
          mFillOval([-.15,-.93], [.15, -.63]);
          this.setOutPortValue(value);
