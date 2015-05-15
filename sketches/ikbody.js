b9dd8027714bff9f2eaafb55e521f04e59e24ea0
kenperlin <kenperlin@gmail.com>
2015-05-15 19:30:27 +0200

    Added diagnostic output to ikbody.js to measure noise in hand orientation.

--- sketches/ikbody.js
+++ sketches/ikbody.js
@@ -2,19 +2,38 @@ function() {
    this.label = "ikbody";
    this.ikBody = new IKBody(ik_data);
 
+   this.createMesh = function() {
+      return this.ikBody.mesh;
+   }
    this.render = function() {
       this.code = [];
       this.duringSketch(function() {
-         mLine([-1,.5],[  1,.5]);
-         mLine([ 0, 1],[  0, 0]);
-         mLine([ 0, 0],[-.5,-1]);
-         mLine([ 0, 0],[ .5,-1]);
+         mLine([-1,.5],[1,.5]);
+         mCurve([[0,1],[0,0],[-.5,-1]]);
+         mLine([0,0],[.5,-1]);
       });
       this.afterSketch(function() {
-         this.ikBody.render(time);
+         var ikb = this.ikBody;
+         ikb.render(time);
+
+// DIAGNOSTIC OUTPUT OF VARIATIONS IN HAND ORIENTATION:
+
+	 function val(f) {
+	    f = max(0,min(ikb.nFrames() - 1, f));
+	    return abs(ikb.getQ(f, 3).w);
+         }
+
+	 var frame = ikb.getFrame(time);
+         var sum = 0, count = 0;
+	 for (var f = frame - 10 ; f <= frame + 10 ; f++) {
+	    sum += val(f);
+	    count++;
+	 }
+	 var diff = val(frame) - sum / count;
+
+	 this.setOutPortValue(diff);
+
+//////////////////////////////////////////////////////
       });
-   }
-   this.createMesh = function() {
-      return this.ikBody.mesh;
    }
 }
