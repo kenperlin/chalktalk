f1effa3965b8078c9cdda6120b2945873ef91071
kenperlin <kenperlin@gmail.com>
2015-06-05 08:15:57 +0200

    Bar charts with lots of functionality -- can be both input and output. Face can get input now as well as output. Better optional display of data for sketchLink. Fixed bug in lineWidth during fade-up of afterSketch lines. Added sketch.isAfterSketch() method. Defined bias and gain functions in utility.js.

--- sketches/face.js
+++ sketches/face.js
@@ -2,7 +2,8 @@ function() {
    this.label = 'face';
    this.is3D = true;
    this.mode = 0;
-   this.onRelease = function() { this.mode++; }
+   this.isAnimated = false;
+   this.onCmdClick = function() { this.isAnimated = ! this.isAnimated; }
    this.render = function() {
       function p(x,y,s) {
          var z = sqrt(1 - x * x - y * y);
@@ -18,41 +19,54 @@ function() {
          mDrawOval([-1,-1],[1,1],32,PI/2,PI/2-TAU);
       });
 
-      function _noise(t) { return max(-1, min(1, 3 * noise(.8 * t))); }
+      function _noise(t) { return max(-1, min(1, 3 * noise(.2 * t))); }
 
       var a = -1, d = 0, e = -1, h = 0, i = 0, o = 0, s = -1, x = 0, y = 0, z = 0;
       this.afterSketch(function() {
 
-         if (this.mode == 0)
+         if (! this.isAnimated)
 	    this.startTime = undefined;
 
-         if (this.mode > 1) {
+         if (this.isAnimated) {
             if (this.startTime === undefined)
 	       this.startTime = time;
 
 	    var t = time - this.startTime;
-            a = _noise(t * 3 + 10);
-            d = _noise(t     + 20);
-            e = _noise(t     + 30);
-            h = _noise(t     + 40);
-            i = _noise(t     + 50);
-            o = _noise(t     + 60);
-            s = _noise(t     + 70);
-            x = _noise(t     + 80);
-            y = _noise(t     + 90);
-            z = _noise(t     + 100);
+            a = _noise(t * 3    +  10.1);
+            d = _noise(t * 1.01 +  20.2);
+            e = _noise(t * 1.02 +  30.3);
+            h = _noise(t * 1.03 +  40.4);
+            i = _noise(t * 1.04 +  50.5);
+            o = _noise(t * 1.05 +  60.6);
+            s = _noise(t * 1.06 +  70.7);
+            x = _noise(t * 1.07 +  80.8);
+            y = _noise(t * 1.08 +  90.9);
+            z = _noise(t * 1.09 + 100.0);
          }
+
+	 else if (this.isInValueAt(0)) {
+	    a = def(this.inValues[0], a);
+	    d = def(this.inValues[1], d);
+	    e = def(this.inValues[2], e);
+	    h = def(this.inValues[3], h);
+	    i = def(this.inValues[4], i);
+	    o = def(this.inValues[5], o);
+	    s = def(this.inValues[6], s);
+	    x = def(this.inValues[7], x);
+	    y = def(this.inValues[8], y);
+	    z = def(this.inValues[9], z);
+	 }
       });
-      var A = .1  * gain(.5 + .5 * a, .7);
-      var D = .07 * d;
-      var E = .07 * (.5 + .5 * e);
-      var H = .05 * h;
-      var I = .05 * i;
-      var O = .1  * o;
-      var S = .06 * gain(bias(.5 + .5 * s, .1), .9);
-      var X = .2 * x;
-      var Y = .5 * y;
-      var Z = .1 * z;
+      var A = max(0, (.5 + .5*a) * .15 - .05);       // Ah
+      var D = .07 * d;                               // Dominant
+      var E = .07 * (.5 + .5 * e);                   // Eyes open
+      var H = .05 * h;                               // Happy
+      var I = .05 * i;                               // Interest
+      var O = .1  * o;                               // Ooh
+      var S = .06 * gain(bias(.5 + .5 * s, .1), .9); // Sneer
+      var X = .2  * x;                               // X rotate
+      var Y = .5  * y;                               // Y rotate
+      var Z = .1  * z;                               // Z rotate
 
       this.extendBounds([[-1.1,-.8],[1.1,.6]]);
 
@@ -69,10 +83,10 @@ function() {
 
       lineWidth(1);
 
-      mSpline([p(-.2+O  ,-.5+H          ),
-               p(-.1+O/2,-.5-H*.35-A*.75),
-               p( .1-O/2,-.5-H*.35-A*.75),
-               p( .2-O  ,-.5+H          )]);
+      mSpline([p(-.2+O  ,-.45+H          ),
+               p(-.1+O/2,-.45-H*.35-A*.75),
+               p( .1-O/2,-.45-H*.35-A*.75),
+               p( .2-O  ,-.45+H          )]);
 
       this.afterSketch(function() {
 
@@ -88,10 +102,10 @@ function() {
 
          mSpline([p(-.1,-.2), p(0,-.2,1.05), p(.1,-.2)]);
 
-         mSpline([p(-.2+O    ,-.5+H              ),
-                  p(-.1+O/2-S,-.5-H*.35+A*.75 + S),
-                  p( .1-O/2+S,-.5-H*.35+A*.75 + S),
-                  p( .2-O    ,-.5+H              )]);
+         mSpline([p(-.2+O    ,-.45+H              ),
+                  p(-.1+O/2-S,-.45-H*.35+A*.75 + S),
+                  p( .1-O/2+S,-.45-H*.35+A*.75 + S),
+                  p( .2-O    ,-.45+H              )]);
 
          this.setOutPortValue([a,d,e,h,i,o,s,x,y,z]);
       });
--- sketches/zbarchart.js
+++ sketches/zbarchart.js
@@ -1,27 +1,91 @@
 function() {
    this.labels = 'barcharth barchartv'.split(' ');
-   this.onClick = function() { this.selection = 1 - this.selection; }
+   this.onCmdClick = function() { this.selection = 1 - this.selection; }
+   this.isHorizontal = function() { return this.getLabel() == 'barcharth'; }
+   this.onPress = function(p) {
+      if (! this.isInValueAt(0)) {
+         var u = this.isHorizontal() ? -p.y : p.x;
+         this.valueIndex = floor(this.nValues * (.5 + .499 * u));
+      }
+   }
+   this.onDrag = function(p) {
+      if (this.valueIndex >= 0) {
+         var v = this.isHorizontal() ? p.x : p.y;
+	 this.values[this.valueIndex] = max(-1, min(1, v));
+      }
+   }
+   this.onRelease = function(p) {
+      this.valueIndex = -1;
+   }
+   this.onCmdPress = function(p) {
+      if (this.pDrag === undefined)
+         this.pDrag = newVec3();
+      this.pDrag.copy(p);
+   }
+   this.onCmdDrag = function(p) {
+      var dx = p.x - this.pDrag.x;
+      if (abs(dx) > 0.2) {
+         this.nValues += dx > 0 ? 1 : this.nValues > 1 ? -1 : 0;
+	 this.pDrag.copy(p);
+      }
+   }
+   this.nValues = 1;
+   this.valueIndex = -1;
+   this.values = newArray(100);
+   this.displayMode = function() {
+      var link = this.findInLink(0);
+      if (isDef(link))
+         return link.displayMode();
+      return 0;
+   }
+   this.displayIndex = function() {
+      var link = this.findInLink(0);
+      if (isDef(link))
+         return link.displayIndex();
+      return 0;
+   }
    this.render = function() {
-      mLine([-1,1],[-1,-1]);
-      mLine([-1,-1],[1,-1]);
+      switch (this.getLabel()) {
+      case 'barcharth':
+         mLine([1,1],[-1,1]);
+         mLine([-1,1],[-1,-1]);
+	 break;
+      case 'barchartv':
+         mLine([-1,1],[-1,-1]);
+         mLine([-1,-1],[1,-1]);
+	 break;
+      }
       this.duringSketch(function() {
-         switch (this.labels[this.selection]) {
-         case 'barcharth': mCurve([[-1,.25],[.1,.25],[.1,-.25],[-1,-.25]]); break;
-         case 'barchartv': mCurve([[-.25,-1],[-.25,.1],[.25,.1],[.25,-1]]); break;
+         switch (this.getLabel()) {
+         case 'barcharth':
+	    mCurve([[-1,.5],[0,.5],[0,-.5],[-1,-.5]]);
+	    break;
+         case 'barchartv':
+	    mCurve([[-.5,-1],[-.5,0],[.5,0],[.5,-1]]);
+	    break;
 	 }
       });
       this.afterSketch(function() {
-         var v = this.inValues;
-         var n = v.length;
+         var isInput = this.isInValueAt(0);
+         var v  = isInput ? this.inValues : this.values;
+         var n  = isInput ? v.length : this.nValues;
+	 var ii = isInput ? this.displayMode() > 0 ? this.displayIndex() : -1 : this.valueIndex;
+
 	 for (var i = 0 ; i < n ; i++) {
-	    color(scrimColor(i % 2 == 0 ? 0.2 : 0.4));
-	    var t0 = mix(-1, 1, (i+.1) / n);
-	    var t1 = mix(-1, 1, (i+.9) / n);
+	    color(i==ii ? liveDataColor : scrimColor(i % 2 == 0 ? 0.5 : 0.3));
+	    var t0 = mix(-1, 1, (i+.25) / n);
+	    var t1 = mix(-1, 1, (i+.75) / n);
             switch (this.labels[this.selection]) {
 	    case 'barcharth': mFillCurve([[-1,-t0],[v[i],-t0],[v[i],-t1],[-1,-t1]]); break;
 	    case 'barchartv': mFillCurve([[ t0,-1],[ t0,v[i]],[ t1,v[i]],[ t1,-1]]); break;
 	    }
 	 }
+
+	 if (this._opv === undefined || this._opv.length != n)
+	    this._opv = newArray(n);
+         for (var i = 0 ; i < n ; i++)
+	    this._opv[i] = v[i];
+         this.setOutPortValue(this._opv);
       });
    }
 }
