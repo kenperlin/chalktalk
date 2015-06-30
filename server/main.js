df6711009e7a7ff1e7186a586d5f45a16865cfa2
kenperlin <kenperlin@gmail.com>
2015-06-30 13:21:09 -0400

    Ongoing experiments in multi-client chalktalk.

--- server/main.js
+++ server/main.js
@@ -39,6 +39,22 @@ app.route("/upload").post(function(req, res, next) {
    });
 });
 
+var values = {};
+
+app.route("/setValue").post(function(req, res, next) {
+   var form = formidable.IncomingForm();
+   form.parse(req, function(err, fields, files) {
+      values[fields.key] = fields.value;
+   });
+});
+
+app.route("/getValue").post(function(req, res, next) {
+   var form = formidable.IncomingForm();
+   form.parse(req, function(err, fields, files) {
+      returnString(res, values[fields.key]);
+   });
+});
+
 app.route("/set").post(function(req, res, next) {
    var form = formidable.IncomingForm();
    form.parse(req, function(err, fields, files) {
@@ -67,6 +83,14 @@ app.route("/talk").get(function(req, res) {
    res.sendfile("index.html");
 });
 
+var time = 0;
+
+// handle request for the current time
+app.route("/getTime").get(function(req, res) {
+   time = (new Date()).getTime();
+   returnString(res, '' + time);
+});
+
 // handle request for list of available sketches
 app.route("/ls_sketches").get(function(req, res) {
    readDir(res, "sketches");
@@ -76,6 +100,12 @@ app.route("/ls_sketches").get(function(req, res) {
 app.route("/ls_images").get(function(req, res) {
    readDir(res, "images");
 });
+
+function returnString(res, str) {
+   res.writeHead(200, { "Content-Type": "text/plain" });
+   res.write(str + "\n");
+   res.end();
+};
 
 function readDir(res, dirName) {
    fs.readdir("./" + dirName + "/", function(err, files) {
@@ -161,21 +191,17 @@ try {
    var wss = new WebSocketServer({ port: 22346 });
    var timeline = [];
 
-   wss.broadcast = function(sender, message) {
-      wss.clients.forEach(function(client) {
-         // DON'T BROADCAST MESSAGES BACK TO THE ORIGINAL SENDER
-         if (client != sender)
-            client.send(message);
-      });
-   };
-
-   wss.on("connection", function(socket) {
-      console.log("new connection");
-
-      socket.on("message", function(message) {
-         //console.log("got message: " + message);
+   wss.on("connection", function(client) {
+      client.i = 0;
+      client.on("message", function(message) {
+         console.log(message);
+         var client_n;
          timeline.push(message);
-         wss.broadcast(socket, message);
+	 for (var n = 0 ; n < wss.clients.length ; n++) {
+	    client_n = wss.clients[n];
+	    while (client_n.i < timeline.length)
+	       client_n.send(timeline[client_n.i++]);
+         }
       });
    });
 } catch (err) {
