77e8dc2f52914bf502e4a92a8b94f9b890e8d371
kenperlin <kenperlin@gmail.com>
2015-07-12 18:16:11 -0700

    Added sketches/flap.js to replace 'flap' selection removed from sketches/diagram.js. Added sketches/xyzr.js to replace 'circle' selection removed from sketches/diagram.js.

---
+++ sketches/flap.js
@@ -0,0 +1,66 @@
+function() {
+   this.label = 'flap';
+
+   this.isWandering = false;
+   this.isExiting = false;
+
+   this.swipe[2] = ['exit', function() { this.exitTime = time; this.isExiting = ! this.isExiting; }];
+   this.swipe[4] = ['wander', function() { this.wanderTime = time; this.isWandering = ! this.isWandering; }];
+
+   this.render = function() {
+
+      this.initCopy = function() { this.exitDx = undefined; }
+
+      var phase = 1;
+      if (this.phaseOffset === undefined)
+         this.phaseOffset = TAU * random();
+      else
+         phase = this.phaseOffset + 8 * time + 1;
+      if (isNaN(phase)) phase = 1;
+
+      var a =  .2+.5 * sin(phase);
+      var b = -.3-.5 * cos(phase);
+
+      var ca = cos(a), cb = cos(a+b);
+      var sa = sin(a), sb = sin(a+b);
+
+      var ox = 0, oy = 0;
+      if (this.isWandering) {
+         var t = time - this.wanderTime;
+         ox =      noise2(t/2, 10);
+         oy = .5 * noise2(t  , 20);
+      }
+      if (this.isExiting) {
+         if (this.exitDx === undefined)
+            this.exitDx = 2 * random() - 1;
+
+         var t = time - this.exitTime;
+         ox += .5 * t * t * this.exitDx;
+         oy += .5 * t;
+      }
+
+      // WHEN BIRD DISAPPEARS FROM VIEW, GET RID OF IT.
+
+      if (this.cy() < -width()/8)
+         this.fadeAway = 1;
+
+      var dx1 = .25, dy1 = 0;
+      var dx2 = .25, dy2 = 0;
+
+      var x0 = 0, y0 = -.05 * cos(phase);
+
+      var x1 = x0 + (dx1 * ca + dy1 * sa);
+      var y1 = y0 + (dx1 * sa - dy1 * ca);
+
+      var x2 = x1 + (dx2 * cb + dy2 * sb);
+      var y2 = y1 + (dx2 * sb - dy2 * cb);
+
+      mCurve([ [ox-x0, oy+y0], [ox-x1, oy+y1] ]);
+      mCurve([ [ox-x1, oy+y1], [ox-x2, oy+y2] ]);
+      mCurve([ [ox+x0, oy+y0], [ox+x1, oy+y1] ]);
+      mCurve([ [ox+x1, oy+y1], [ox+x2, oy+y2] ]);
+
+      this.extendBounds([[-.5,-.26],[.5,.31]]);
+   }
+}
+
---
+++ sketches/xyzr.js
@@ -0,0 +1,28 @@
+function() {
+   this.label = 'xyzr';
+   this.show_xyzr = false;
+   this.show_sn   = false;
+
+   this.swipe[1] = ['show\nx,y,z and r', function() { this.show_xyzr = true; }];
+   this.swipe[3] = ['show\nS and N'    , function() { this.show_sn   = true; }];
+
+   this.render = function() {
+      var X = cos(PI/4), Y = sin(PI/4);
+
+      textHeight(this.mScale(0.1));
+      mCurve(arc());
+
+      if (this.show_xyzr) {
+         mLine([0, 0], [X, Y]);
+         mText('x,y,z', [0, 0], .5, 0);
+         mText('r', [X*.45, Y*.45], 1.3, 1.2);
+      }
+
+      if (this.show_sn) {
+         mArrow([-X, Y], [-X*1.5, Y*1.5]);
+         mText('N', [-X*1.62, Y*1.62], .5, .5);
+         mText('S', [-X, Y], -.25, -.25);
+      }
+   }
+}
+
