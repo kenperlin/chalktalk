398995770e7ae56d6c1d7a5f5906e2f7987ac90b
Ken Perlin <kenperlin@gmail.com>
2015-07-14 18:30:16 -0700

    Slight clean-up of cursor picker logic. Remove rocket sketch after it flies off screen.

--- sketches/rocket.js
+++ sketches/rocket.js
@@ -7,34 +7,28 @@ function() {
    this.swipe[2] = ['BLAST OFF!', function() { this.swipeTime = time + 0.5; }];
 
    this.render = function(elapsed) {
-      if (this.swipeTime > 0 && time > this.swipeTime) {
+      if (this.yhi < 0)                                  // REMOVE WHEN GONE
+         sketchToDelete = this;
+
+      if (this.swipeTime > 0 && time > this.swipeTime) { // AFTER SWIPE
          this.velocity += 0.4 * elapsed;
-         this.altitude += this.velocity;
+         this.altitude += this.velocity;                    // ACCELERATE
       }
+      m.rotateZ(-0.02 * this.altitude);                  // ANIMATE TRAJECTORY
+      m.translate(0, this.altitude, 0);
 
-      // ANIMATE ROCKET TRAJECTORY
-
-      var t = this.altitude;
-      m.rotateZ(-0.02 * t);
-      m.translate(0, t, 0);
-
-      // DRAW FUSILAGE
-
-      mCurve(makeSpline([[-.25,-1],[-.4,0],[0,1]]));
+      mCurve(makeSpline([[-.25,-1],[-.4,0],[0,1]]));     // DRAW FUSILAGE
       mCurve(makeSpline([[0,1],[ .4,0],[ .25,-1]]));
 
-      // DRAW WINGS
-
-      mCurve([[-.4,0],[-.8,-.8],[-.35,-.8]]);
+      mCurve([[-.4,0],[-.8,-.8],[-.35,-.8]]);            // DRAW WINGS
       mCurve([[ .4,0],[ .8,-.8],[ .35,-.8]]);
 
-      // THRUSTER FLAMES
-
-      this.afterSketch(function(elapsed) {
+      this.afterSketch(function(elapsed) {               // THRUSTER FLAMES
          if (this.swipeTime != 0) {
             mCurve([[-.20, -1.10], [-.3, -1.4 + .7*noise2(10*time, 100)], 
                     [-.08, -1.15], [  0, -1.6 + .7*noise2(10*time, 200)], 
-                    [ .08, -1.15], [ .3, -1.4 + .7*noise2(10*time, 300)], [ .2, -1.1]]);
+                    [ .08, -1.15], [ .3, -1.4 + .7*noise2(10*time, 300)],
+		    [ .20, -1.10]]);
          }
       });
    }
