5a66049eb3d609b5f9fa3d913541febc70761345
kenperlin <kenperlin@gmail.com>
2015-07-26 08:25:40 -0700

    Fixed bug whereby a SimpleSketch at the cursor was not capturing typed text characters. Removed '[' and ']' temporary options for saving and loading a page.

--- sketches/texturexyz.js
+++
@@ -1,7 +0,0 @@
-function() {
-   this.label = 'texturexyz';
-   this.code = [['','texture = f(x,y,z)']];
-   this.render = function() {
-      mClosedCurve(makeSpline([[0,1],[1,0],[.3,-.8],[-.3,-1],[-1,-.2],[-.5,.2],[-.5,.9],[0,1]]));
-   };
-}
