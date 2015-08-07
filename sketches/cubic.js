d3d0ab0d8bfbe76d286caa7a5c510562935dc384
kenperlin <kenperlin@gmail.com>
2015-08-07 01:02:41 -0700

    Reduced font size for text in sketches/cubic.js. Added gesture to save edited code of a sketch (click to right, then swipe right).

--- sketches/cubic.js
+++ sketches/cubic.js
@@ -1,9 +1,9 @@
 function() {
    this.label = 'cubic';
-   this.setSketchText(0, '8.0', [0,.9], .1);
-   this.setSketchText(1, '-12.0', [0,.3], .1);
-   this.setSketchText(2, '6.0', [0,-.3], .1);
-   this.setSketchText(3, '-1.0', [0,-.9], .1);
+   this.setSketchText(0, '1.0', [-.65, .9], .07);
+   this.setSketchText(1, '0.0', [-.65, .3], .07);
+   this.setSketchText(2, '0.0', [-.65,-.3], .07);
+   this.setSketchText(3, '0.0', [-.65,-.9], .07);
    this.func = function(C, t) { return C[0] * t * t * t +
                                        C[1] * t * t +
                                        C[2] * t +
@@ -14,19 +14,25 @@ function() {
       for (var n = 0 ; n < 4 ; n++)
          C.push(parseFloat(this.sketchTexts[n].value));
 
+      lineWidth(1);
       mLine([-1,0],[1,0]);
+      lineWidth(3);
       var curve = [];
-      for (var t = 0 ; t <= 1.001; t += .01)
-         curve.push([2*t-1, this.func(C, t)]);
+      for (var t = -1 ; t <= 1.001; t += .02)
+         curve.push([t, this.func(C, t)]);
       color('red');
       mCurve(curve);
       color(backgroundColor == 'black' ? 'cyan' : 'blue');
       this.afterSketch(function() {
+         color(defaultPenColor);
+         lineWidth(1);
+         mLine([0,-1],[0,1]);
+         color(backgroundColor == 'black' ? 'cyan' : 'blue');
          textHeight(this.mScale(.15));
-	 mText('t'+EXP_3, [1.1, .9], 0,.5);
-	 mText('t'+EXP_2, [1.1, .3], 0,.5);
-	 mText('t'      , [1.1,-.3], 0,.5);
-	 mText('1'      , [1.1,-.9], 0,.5);
+	 mText('a =', [-1.15, .9], 0,.5);
+	 mText('b =', [-1.15, .3], 0,.5);
+	 mText('c =', [-1.15,-.3], 0,.5);
+	 mText('d =', [-1.15,-.9], 0,.5);
 	 color('red');
 	 mText('at' + EXP_3 + ' + bt' + EXP_2 + ' ct + d', [0,-1.2], .5, .5);
       });
