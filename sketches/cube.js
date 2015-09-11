08f12f109502445ac950bcfad0fb19c619f029fd
kenperlin <kenperlin@gmail.com>
2015-09-11 06:29:12 -0400

    cube sketch will now output its shape across a data link.

--- sketches/cube.js
+++ sketches/cube.js
@@ -3,20 +3,24 @@ function() {
    this.is3D = true;
 
    function s(i) { return i > 0 ? 1 : -1; }
+
    var v = [];
    for (var n = 0 ; n < 8 ; n++)
       v.push([s(n&1), s(n&2), s(n&4)]);
 
-   var a = [0,2,4,6, 0,1,4,5, 0,1,2,3];
-   var b = [1,3,5,7, 2,3,6,7, 4,5,6,7];
+   v.edges = [ [0,1], [2,3], [4,5], [6,7],
+               [0,2], [1,3], [4,6], [5,7],
+	       [0,4], [5,1], [2,6], [3,7] ];
 
    this.render = function(elapsed) {
      this.duringSketch(function() {
          mClosedCurve([v[0],v[2],v[3],v[1]]);
       });
       this.afterSketch(function() {
-         for (var n = 0 ; n < a.length ; n++)
-            mLine(v[a[n]], v[b[n]]);
+         for (var n = 0 ; n < v.edges.length ; n++)
+            mLine(v[v.edges[n][0]], v[v.edges[n][1]]);
       });
    }
+
+   this.output = function() { return v; }
 }
