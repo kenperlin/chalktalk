bb22ddb8a8c7565ebef87de9bb5c9f3ea09827ea
kenperlin <kenperlin@gmail.com>
2015-10-09 01:41:02 -0400

    Added sketches/square.js to explain vertex/edge structure in computer graphics.

---
+++ sketches/square.js
@@ -0,0 +1,34 @@
+function() {
+   this.label = 'square';
+   this.mode = 0;
+
+   this.onClick = function() { this.mode++; }
+
+   this.render = function() {
+      mLine([-1,-1],[ 1,-1]);
+      mLine([ 1,-1],[ 1, 1]);
+      mLine([ 1, 1],[-1, 1]);
+      mLine([-1, 1],[-1,-1]);
+
+      this.afterSketch(function() {
+         textHeight(this.mScale(0.2));
+	 if (this.mode > 0) {
+	    mText("-1,-1", [-1.2,-1.2], .5, .5);
+	    mText("1,-1", [ 1.2,-1.2], .5, .5);
+	    mText("-1,1", [-1.2, 1.2], .5, .5);
+	    mText("1,1", [ 1.2, 1.2], .5, .5);
+         }
+
+	 if (this.mode > 1) {
+	    mText("0: -1,-1\n 1:   1,-1\n2:   1,  1\n3: -1,  1", [-.3,0], 0, .5);
+         }
+
+	 if (this.mode > 2) {
+	    mText("[0,1]", [0,-1.2], .5, .5);
+	    mText("[1,2]", [1.3, 0], .5, .5);
+	    mText("[2,3]", [0, 1.2], .5, .5);
+	    mText("[3,0]", [-1.3, 0], .5, .5);
+         }
+      });
+   }
+}
