a67251dd554ad7fe84cd8174004c8ae677f88fc7
kenperlin <kenperlin@gmail.com>
2015-12-22 22:19:56 -0500

    Got rid of vestiges of rightGesture and bottomGesture logic (such as bottom pan and page selection). Modified sketches/cubic.js so that it works properly to illustrate principles of Bezier and Hermite sketches. Removed file lib/strokesManager.js, since it is no longer used for anything.

--- sketches/cubic.js
+++ sketches/cubic.js
@@ -1,9 +1,9 @@
 function() {
    this.label = 'cubic';
-   this.setSketchText(0, '1.0', [-.65, .9], .07);
-   this.setSketchText(1, '0.0', [-.65, .3], .07);
-   this.setSketchText(2, '0.0', [-.65,-.3], .07);
-   this.setSketchText(3, '0.0', [-.65,-.9], .07);
+   this.setSketchText(0, '1.0', [-1.15, .9], .07);
+   this.setSketchText(1, '0.0', [-1.15, .3], .07);
+   this.setSketchText(2, '0.0', [-1.15,-.3], .07);
+   this.setSketchText(3, '0.0', [-1.15,-.9], .07);
    this.func = function(C, t) { return C[0] * t * t * t +
                                        C[1] * t * t +
                                        C[2] * t +
@@ -15,24 +15,27 @@ function() {
          C.push(parseFloat(this.sketchTexts[n].value));
 
       lineWidth(1);
-      mLine([-1,0],[1,0]);
+      mLine([-1,-1],[1,-1]);
       lineWidth(3);
       var curve = [];
-      for (var t = -1 ; t <= 1.001; t += .02)
-         curve.push([t, this.func(C, t)]);
+      for (var t = 0 ; t <= 1.001; t += .01)
+         curve.push([t*2-1, this.func(C, t)*2-1]);
       color('red');
       mCurve(curve);
       color(backgroundColor == 'black' ? 'cyan' : 'blue');
       this.afterSketch(function() {
          color(defaultPenColor);
          lineWidth(1);
-         mLine([0,-1],[0,1]);
+         mLine([-1,-1],[-1,1]);
          color(backgroundColor == 'black' ? 'cyan' : 'blue');
          textHeight(this.mScale(.15));
-         mText('a =', [-1.15, .9], 0,.5);
-         mText('b =', [-1.15, .3], 0,.5);
-         mText('c =', [-1.15,-.3], 0,.5);
-         mText('d =', [-1.15,-.9], 0,.5);
+         mText('a =', [-1.65, .9], 0,.5);
+         mText('b =', [-1.65, .3], 0,.5);
+         mText('c =', [-1.65,-.3], 0,.5);
+         mText('d =', [-1.65,-.9], 0,.5);
+	 for (var i = 0 ; i < 4 ; i++)
+	    if (this.inValues[i] !== undefined)
+	       this.sketchTexts[i].setValue(roundedString(this.inValues[i]));
          color('red');
          mText('at' + EXP_3 + ' + bt' + EXP_2 + ' ct + d', [0,-1.2], .5, .5);
       });
