276397c50185a51b925761450f94999c7df36519
kenperlin <kenperlin@gmail.com>
2016-01-15 11:07:35 -0500

    Made minor appearance improvements to 'y' and '&' characters in lib/CT_LineFont.js. Changed scale and translation of figure in sketches/ascii.js to approximately fit in a -1...+1 square.

--- sketches/ascii.js
+++ sketches/ascii.js
@@ -1,6 +1,8 @@
 function() {
    this.label = 'ascii';
    this.render = function() {
+      m.scale(1/16);
+      m.translate(16, 3.2 * 3, 0);
       for (var i = 0 ; i < CT.lineFont.length ; i++) {
          if (i % 16 == 0)
 	    m.translate(-32, -3.2, 0);
