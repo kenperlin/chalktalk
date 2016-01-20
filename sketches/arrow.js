c1ed31d8a88d785a408e13e44ffe1ca960da2a5b
kenperlin <kenperlin@gmail.com>
2016-01-20 06:52:15 -0500

    Changed column size in lib/glyphChart.js to 11. Added ability to change size of arrowhead in sketches/arrow.js by dragging up and down. Added sketches/line.js.

--- sketches/arrow.js
+++ sketches/arrow.js
@@ -1,22 +1,29 @@
 function() {
    this.labels = 'larrow rarrow uarrow darrow'.split(' ');
+   this.H = .5;
+   this.onPress = function(p) { this.py = p.y; }
+   this.onDrag = function(p) {
+      this.H += p.y - this.py;
+      this.py = p.y;
+   }
    this.render = function() {
+      var H = this.H;
       lineWidth(6);
       switch (this.labels[this.selection]) {
       case 'larrow':
-         mCurve([[-.5,.5],[-1,0],[-.5,-.5]]);
+         mCurve([[H-1,H],[-1,0],[H-1,-H]]);
          mLine([-1,0],[1,0]);
 	 break;
       case 'rarrow':
-         mCurve([[.5,.5],[1,0],[.5,-.5]]);
+         mCurve([[1-H,H],[1,0],[1-H,-H]]);
          mLine([1,0],[-1,0]);
 	 break;
       case 'uarrow':
-         mCurve([[-.5,.5],[0,1],[.5,.5]]);
+         mCurve([[-H,1-H],[0,1],[H,1-H]]);
 	 mLine([0,1],[0,-1]);
 	 break;
       case 'darrow':
-         mCurve([[-.5,-.5],[0,-1],[.5,-.5]]);
+         mCurve([[-H,H-1],[0,-1],[H,H-1]]);
 	 mLine([0,-1],[0,1]);
 	 break;
       }
---
+++ sketches/line.js
@@ -0,0 +1,6 @@
+function() {
+  this.label = 'line';
+  this.render = function() {
+     mLine([-1,0], [1,0]);
+  }
+}
