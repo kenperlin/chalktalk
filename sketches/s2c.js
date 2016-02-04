50d410f2090e21973be506af8aa9afa29efe34fb
kenperlin <kenperlin@gmail.com>
2016-02-04 12:48:12 -0500

    Added TT Sensor data reading to the server and to lib/g.js. Added ability to drag to modify x,y scale of cube.

--- sketches/s2c.js
+++ sketches/s2c.js
@@ -1,7 +1,18 @@
 function() {
    this.label = "s2c";
+   this._scaleX = 1;
+   this._scaleY = 1;
+   this._p = newVec3();
+
+   this.onPress = function(p) { this._p.copy(p); }
+   this.onDrag = function(p) {
+      this._scaleX += p.x - this._p.x;
+      this._scaleY += p.y - this._p.y;
+      this._p.copy(p);
+   }
 
    this.render = function() {
+      m.scale(this._scaleX, this._scaleY, 1);
       mCube();
       this.duringSketch(function() {
          mCurve([ [-1,-1,1],[1,-1,1],[1,1,1],[-1,1,1],[-1,-1,1] ]);
