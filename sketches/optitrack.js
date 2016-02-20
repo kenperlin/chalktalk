11917e3a088d45eaced561c4fcd3c35ec47f7dae
kenperlin <kenperlin@gmail.com>
2016-02-20 16:56:07 -0500

    Added a lower level access method for lib/midi.js. Modified lib/glyphChart.js to optimize for fast rendering.

--- sketches/optitrack.js
+++
@@ -1,7 +0,0 @@
-function() {
-   this.label = 'optitrack';
-   this.render = function() {
-      mClosedCurve([[-.7,1],[.7,1],[.7,-1],[-.7,-1]]);
-      mCurve([[.35,-1],[.35,-1.5],[-.35,-1.5],[-.35,-1]]);
-   }
-}
