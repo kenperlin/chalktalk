fd4be427006fd1b0ffabfa095d59b36ec053b436
kenperlin <kenperlin@gmail.com>
2016-09-29 17:37:04 -0400

    Added most recent changes.  Most important: added lib/faders.js.

--- sketches/boolean.js
+++ sketches/boolean.js
@@ -6,12 +6,12 @@ function() {
    this.render = function() {
       lineWidth(this.mScale(0.2));
       if (this.output()) {
-         mLine([-.9,1],[.9, 1]);
          mLine([  0,1],[ 0,-1]);
+         mLine([-.9,1],[.9, 1]);
       }
       else {
-         mCurve([[-.5,-1],[-.5,1],[.7,1]]);
-         mLine(  [-.5, 0],[ .1,0]);
+         mCurve([[.7,1],[-.5,1],[-.5,-1]]);
+         mLine([-.5, 0],[ .1,0]);
       }
    }
    this.output = function() { return def(this.inValue[0], this.state) > 0 ? 1 : 0; }
