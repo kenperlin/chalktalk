397261cac12c2827c4c5826b286874ad9a2d36dd
Gabriel B. Nunes <gabriel@kronopath.net>
2016-11-10 23:03:19 -0500

    Implement types in the Boolean sketch. Move all other sketches to a subfolder.
    This’ll prevent us from spawning any sketches that don’t use the type system.

--- sketches/boolean.js
+++ sketches/boolean.js
@@ -5,7 +5,7 @@ function() {
    this.onClick = function() { this.state = ! this.state; }
    this.render = function() {
       lineWidth(this.mScale(0.2));
-      if (this.output()) {
+      if (this.output().b) {
          mLine([  0,1],[ 0,-1]);
          mLine([-.9,1],[.9, 1]);
       }
@@ -14,6 +14,21 @@ function() {
          mLine([-.5, 0],[ .1,0]);
       }
    }
-   this.output = function() { return def(this.inValue[0], this.state) > 0 ? 1 : 0; }
+   this.output = function() {
+      // TODO: move some of this type checking to infrastructure, shouldn't have to implement it
+      // in the individual sketches.
+      if (this.inValue[0] instanceof AT.Type) {
+         if (this.inValue[0] instanceof AT.Bool) {
+            return this.inValue[0];
+         }
+         else {
+            let converted = this.inValue[0].convert("Bool");
+            return (converted !== undefined) ? converted : new AT.Bool(this.state);
+         }
+      }
+      else {
+         return new AT.Bool(this.state);
+      }
+   }
 }
 
