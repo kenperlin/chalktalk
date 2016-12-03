7a2d6e3352cab89f135d462913b976fb1a657821
Gabriel B. Nunes <gabriel@kronopath.net>
2016-12-02 20:36:31 -0500

    Added a whole bunch of deprecation markers to variables relating to the old port system

--- sketches/boolean.js
+++ sketches/boolean.js
@@ -17,12 +17,12 @@ function() {
    this.output = function() {
       // TODO: move some of this type checking to infrastructure, shouldn't have to implement it
       // in the individual sketches.
-      if (this.inValue[0] instanceof AT.Type) {
-         if (this.inValue[0] instanceof AT.Bool) {
-            return this.inValue[0];
+      if (this.inValue_DEPRECATED_PORT_SYSTEM[0] instanceof AT.Type) {
+         if (this.inValue_DEPRECATED_PORT_SYSTEM[0] instanceof AT.Bool) {
+            return this.inValue_DEPRECATED_PORT_SYSTEM[0];
          }
          else {
-            let converted = this.inValue[0].convert("Bool");
+            let converted = this.inValue_DEPRECATED_PORT_SYSTEM[0].convert("Bool");
             return (converted !== undefined) ? converted : new AT.Bool(this.state);
          }
       }
