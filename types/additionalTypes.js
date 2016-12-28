3c6dac69033d689137beeba4eb9e599e896304e2
Gabriel B. Nunes <gabriel@kronopath.net>
2016-12-27 22:15:52 -0400

    Added an “unknown type” as the beginning of an experiment with backwards compatibility

--- types/additionalTypes.js
+++ types/additionalTypes.js
@@ -3,4 +3,13 @@
 (function(AT) {
     // Chalktalk-specific type defintions go in this file.
     
+    // Unknown types are meant for sketches and objects that have not been converted to use
+    // Atypical types yet. They're not convertible to or from anything and are just dumb
+    // wrappers around JS variables.
+    AT.UnknownType = AT.defineType({
+        typename: "UnknownType",
+        init: function(val) {
+            this._def("val", val);
+        }
+    })
 })(Atypical);
