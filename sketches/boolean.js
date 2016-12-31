389523075cc9286c06192dc7f7c2cf03bccc1a04
Gabriel B. Nunes <gabriel@kronopath.net>
2016-12-31 15:51:11 -0400

    Started developing a new ports system that is type-aware.
    So far I’ve moved the boolean.js sketch to the new system and converted it enough so that you can connect multiple boolean sketches together and propagate their values. More work incoming.

--- sketches/boolean.js
+++ sketches/boolean.js
@@ -2,10 +2,10 @@
 function() {
    this.label = "Boolean";
    this.state = true;
-   this.onClick = function() { this.state = ! this.state; }
+   this.onClick = function() {this.state = ! this.state;}
    this.render = function() {
       lineWidth(this.mScale(0.2));
-      if (this.output().b) {
+      if (this.output.func().b) {
          mLine([  0,1],[ 0,-1]);
          mLine([-.9,1],[.9, 1]);
       }
@@ -14,21 +14,16 @@ function() {
          mLine([-.5, 0],[ .1,0]);
       }
    }
-   this.output = function() {
-      // TODO: move some of this type checking to infrastructure, shouldn't have to implement it
-      // in the individual sketches.
-      if (this.inValue_DEPRECATED_PORT_SYSTEM[0] instanceof AT.Type) {
-         if (this.inValue_DEPRECATED_PORT_SYSTEM[0] instanceof AT.Bool) {
-            return this.inValue_DEPRECATED_PORT_SYSTEM[0];
+
+   this.defineOutput("Bool",
+      function() {
+         if (this.inputs.hasValue(0)) {
+            return this.inputs.value(0);
          }
-         else {
-            let converted = this.inValue_DEPRECATED_PORT_SYSTEM[0].convert("Bool");
-            return (converted !== undefined) ? converted : new AT.Bool(this.state);
-         }
-      }
-      else {
          return new AT.Bool(this.state);
       }
-   }
+   );
+
+   this.defineInput("Bool");
 }
 
