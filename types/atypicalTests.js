da7637647cb95e2145c5dd3cfd21eb1b7c6b6793
Gabriel B. Nunes <gabriel@kronopath.net>
2017-01-07 14:29:24 -0400

    Use the newly-created test-module-creation function to properly implement tests for Atypical.

--- types/atypicalTests.js
+++ types/atypicalTests.js
@@ -2,71 +2,52 @@
 
 // This file is meant for testing the Atypical type system.
 // To use it, add it to an HTML file WITHOUT adding atypical.js to it. This test will do so instead.
-var AtypicalTests = (function() {
+window.AtypicalTests = (function() {
    var T = {}
 
-   function assert(expression) {
-      console.assert(expression);
-      if (!expression) {
-         testsPassed = false;
-      }
-   }
-
-   function reloadAtypical(completion) {
-      var originalScript = document.getElementById("atypical");
-      if (originalScript) {
-         originalScript.remove();
-      }
-
-      var script = document.createElement("script");
-      script.id = "atypical";
-      script.src = "atypical.js";
-      script.onloadend = completion;
-      document.head.appendChild(script);
-   }
-
-   // Add any new test functions to this array.
-   // The Atypical type system will be reset after every one.
-   var tests = [
-      // Test that defining types works
-      function () {
-         Test1 = AT.defineType({
-            typename: "Test1",
-            init: function(){}
-         });
-         assert(Test1);
-      },
-      // Test that defining duplicate tests does not work
-      function () {
-         Test1 = AT.defineType({
-            typename: "Test1",
-            init: function(){}
-         });
-         assert(Test1);
-         
-         Test1Duplicate = AT.defineType({
-            typename: "Test1",
-            init: function(){}
-         });
-         assert(!Test1Duplicate);
-      },
-   ];
-   
    T.runTests = function(allTestsComplete) {
-      function runTest(i, testComplete) {
-         let passed = tests[i]();
-         reloadAtypical(function() {
-            runTest(i+1, function(nextPassed) {
-               testComplete(passed && nextPassed);
+      // Add any new test functions to this array.
+      // Each should be a function that takes in the type system module.
+      var tests = [
+         // Test that defining types works
+         function (AT) {
+            let Test1 = AT.defineType({
+               typename: "Test1",
+               init: function(){}
             });
-         });
-      }
-      runTest(0, function(testsPassed) {
-         allTestsComplete(testsPassed);
-      });
-   }
+            assert(Test1);
+         },
+         // Test that defining duplicate tests does not work
+         function (AT) {
+            let Test1 = AT.defineType({
+               typename: "Test1",
+               init: function(){}
+            });
+            assert(Test1);
 
-   
+            let Test1Duplicate = AT.defineType({
+               typename: "Test1",
+               init: function(){}
+            });
+            assert(!Test1Duplicate);
+         },
+      ];
+
+      var testsPassed = true;
+
+      function assert(expression) {
+         console.assert(expression);
+         if (!expression) {
+            testsPassed = false;
+         }
+      }
+
+      for (let i = 0; i < tests.length; i++) {
+         tests[i](AT._createTestingModule());
+      }
+
+      return testsPassed;
+   }
 
    return T;
 })()
