c8b296109155347872973882b79666427c095934
Gabriel B. Nunes <gabriel@kronopath.net>
2017-01-07 13:06:04 -0400

    Added a small bit more sophistication to the testing framework

--- types/atypicalTests.js
+++ types/atypicalTests.js
@@ -3,8 +3,22 @@
 var AtypicalTests = (function() {
    var T = {}
 
+   var testsPassed = true;
+
+   function assert(expression) {
+      console.assert(expression);
+      if (!expression) {
+         testsPassed = false;
+      }
+   }
+
    T.runTests = function() {
-      console.assert(false);
+      testsPassed = true;
+
+      assert(1 == 1);
+      assert(1 == 2);
+
+      return testsPassed;
    }
 
    return T;
