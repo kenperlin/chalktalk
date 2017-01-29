ab2c46f5c8ce93683353f8c4dc54ccfacd710772
kenperlin <kenperlin@gmail.com>
2017-01-29 09:33:51 -0500

    Added sketches/f1d.js -- a 1d function that lets the user edit the code that defines the function. Modified lib/sketch.js -- allow first input to evalCode() to be an array.  Result is mapped to all elements of the array.

--- sketches/f1d.js
+++ sketches/f1d.js
@@ -1,5 +1,7 @@
 function() {
    this.label = "F1d";
+   this.isPos = false;
+   this.onSwipe[5] = ['range', function() { this.isPos = ! this.isPos; }];
    this.code = [["", "t\n"]];
    this.y = 0;
 
@@ -12,27 +14,47 @@ function() {
    }
    this.render = function(elapsed) {
       lineWidth(1);
-      mCurve([[-1,0],[1,0]]);
-      mCurve([[0,-1],[0,1]]);
+      if (this.isPos) {
+         mCurve([[-1,-1],[1,-1]]);
+         mCurve([[-1,-1],[-1,1]]);
+      }
+      else {
+         mCurve([[-1,0],[1,0]]);
+         mCurve([[0,-1],[0,1]]);
+      }
 
       var e = 1/100;
       var C = [];
-      for (var t = -1 ; t <= 1 ; t += e) {
+      var t = this.isPos ? 0 : -1;
+      for ( ; t <= 1 ; t += e) {
          var f = this.f(t);
-	 if (! isNaN(f))
-            C.push([t, f]);
+	 if (! isNaN(f)) {
+	    if (this.isPos)
+               C.push([2 * t - 1, 2 * f - 1]);
+            else
+               C.push([t, f]);
+         }
       }
       lineWidth(2);
       mCurve(C);
 
       this.afterSketch(function() {
-         var t = this.getInValue(0, 0);
-         this.y = this.f(t);
-         if (this.y != null) {
-            color(fadedColor(0.5, this.colorId));
-            var tt = max(-1, min(1, t));
-            var yy = max(-1, min(1, this.y));
-            mFillCurve([ [0,0], [tt,0], [tt,yy], [0,yy] ]);
+         if (this.isInValueAt(0)) {
+            var t = this.getInValue(0, 0);
+            this.y = this.f(t);
+            if (this.y != null) {
+	       lineWidth(1);
+               var tt = max(-1, min(1, t));
+               var yy = max(-1, min(1, this.y));
+               var y0 = 0;
+	       if (this.isPos) {
+	          tt = 2 * tt - 1;
+	          yy = 2 * yy - 1;
+	          y0 = 2 * y0 - 1;
+	       }
+               mLine( [tt, y0], [tt, yy] );
+               mFillDisk( [tt, yy], 0.1 );
+            }
          }
       });
    }
