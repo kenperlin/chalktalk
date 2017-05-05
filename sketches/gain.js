2865c251994d303e578ee94112ac0fe5f503fcc5
Gabriel B. Nunes <gabriel@kronopath.net>
2017-05-05 17:10:25 -0400

    Migrated gain to the new ports system

--- sketches/gain.js
+++ sketches/gain.js
@@ -1,25 +1,29 @@
 function() {
-   this.USES_DEPRECATED_PORT_SYSTEM = true;
    this.label = 'gain';
    this.setSketchText(0, '1', [-.3, 0], .2);
+
+   this.defineInput(AT.Function(AT.Seconds, AT.AudioSample));
+   this.defineInput(AT.Function(AT.Seconds, AT.Float));
+
+   this.defineOutput(AT.Function(AT.Seconds, AT.AudioSample), function() {
+      var input = this.inputs.value(0);
+      var gain  = def(this.inputs.value(1), this.sketchTexts[0].value);
+      return function(t) {
+         return valueOf(gain, t) * valueOf(input, t);
+      };
+   });
 
    this.render = function() {
       mCurve([[-1,1],[1,0,],[-1,-1]]);
       mLine([-1,1],[-1,-1]);
       this.afterSketch(function() {
-         if (! (this.sketchTexts[0].isVisible = this.inValue_DEPRECATED_PORT_SYSTEM[1] === undefined)) {
+         let hasControlInput = this.inputs.hasValue(1);
+         this.sketchTexts[0].isVisible = !hasControlInput;
+         if (hasControlInput) {
             m.translate(-.3, 0, 0);
             mCurve(curveForSignal);
          }
       });
-   }
-
-   this.output = function() {
-      var input = this.inValue_DEPRECATED_PORT_SYSTEM[0];
-      var gain  = def(this.inValue_DEPRECATED_PORT_SYSTEM[1], this.sketchTexts[0].value);
-      return function(t) {
-         return valueOf(gain, t) * valueOf(input, t);
-      };
    }
 }
 
