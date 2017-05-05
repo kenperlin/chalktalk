2e9c6c6c9028b40c94ff746a7093c05488feab41
Gabriel B. Nunes <gabriel@kronopath.net>
2017-05-05 16:38:07 -0400

    Migrate “delay” sketch to the new ports system

--- sketches/delay.js
+++ sketches/delay.js
@@ -1,23 +1,27 @@
 function() {
-   this.USES_DEPRECATED_PORT_SYSTEM = true;
    this.label = 'delay';
    this.setSketchText(0, '1', [0, 0], .3);
+
+   this.defineInput(AT.Function(AT.Seconds, AT.AudioSample));
+   this.defineInput(AT.Seconds);
+   this.defineOutput(AT.Function(AT.Seconds, AT.AudioSample), function() {
+      var input = this.inputs.value(0);
+      var delay = def(this.inputs.value(1), this.sketchTexts[0].value);
+      return function(t) {
+         return valueOf(input, t - valueOf(delay, t));
+      }
+   });
 
    this.render = function() {
       mCurve([[-1,.7],[1,.7],[1,-.7],[-1,-.7]]);
       mLine([-1,.7],[-1,-.7]);
       this.afterSketch(function() {
-         if (! (this.sketchTexts[0].isVisible = this.inValue_DEPRECATED_PORT_SYSTEM[1] === undefined))
+         let hasControlInput = this.inputs.hasValue(1);
+         this.sketchTexts[0].isVisible = !hasControlInput;
+         if (hasControlInput) {
             mCurve(curveForSignal);
+         }
       });
-   }
-
-   this.output = function() {
-      var input = this.inValue_DEPRECATED_PORT_SYSTEM[0];
-      var delay = def(this.inValue_DEPRECATED_PORT_SYSTEM[1], this.sketchTexts[0].value);
-      return function(t) {
-         return valueOf(input, t - valueOf(delay, t));
-      }
    }
 }
 
