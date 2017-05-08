8dfd8a5a86735bb0fbf20387dd450339be117481
Gabriel B. Nunes <gabriel@kronopath.net>
2017-05-07 21:02:01 -0400

    Fixed bugs in AT.Mesh’s transform function

--- sketchlibs/meshType.js
+++ sketchlibs/meshType.js
@@ -10,8 +10,12 @@ AT.defineType({
    },
    transform: function(matrix) {
       if (matrix instanceof AT.Matrix) {
-         matrix = matrix.values;
+         matrix = matrix.toFlatArray();
       }
-      return new AT.Mesh(mult(matrix.toFlatArray(), this.mesh));
+      return new AT.Mesh(mult(matrix, this.mesh));
    }
 });
+
+// For use in the matrix sketch, show pairs of matrices and meshes just as matrices
+// when connecting to the background or convering to a string.
+AT.defineConversionsViaIntermediary(AT.Pair(AT.Matrix, AT.Mesh), AT.Matrix, AT.String);
