ab03258c8d8645b24aa69b27284d6e201fe9ee47
Gabriel B. Nunes <gabriel@kronopath.net>
2017-05-12 01:07:56 -0400

    Cleanup and commenting on boolean.js

--- sketches/boolean.js
+++ sketches/boolean.js
@@ -2,14 +2,18 @@
 function() {
    this.label = "Boolean";
    this.state = true;
-   this.onClick = ['toggle T/F', function() { this.state = ! this.state; }];
+   this.onClick = ['toggle T/F',
+      function() { this.state = ! this.state; }];
+
    this.render = function() {
       lineWidth(this.mScale(0.2));
       if (this.output.func()) {
+         // Draw a T
          mLine([  0,1],[ 0,-1]);
          mLine([-.9,1],[.9, 1]);
       }
       else {
+         // Draw an F
          mCurve([[.7,1],[-.5,1],[-.5,-1]]);
          mLine([-.5, 0],[ .1,0]);
       }
