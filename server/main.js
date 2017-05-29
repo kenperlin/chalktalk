017390843e876744092057e6c2725686c91d402a
KTRosenberg <ktr254@nyu.edu>
2017-05-29 18:49:13 -0400

    prevents crash when state directory not present

--- server/main.js
+++ server/main.js
@@ -139,7 +139,7 @@ function readDir(res, dirName, extension) {
    fs.readdir("./" + dirName + "/", function(err, files) {
       if (err) {
          res.writeHead(500, { "Content-Type": "text/plain" });
-         res.write(err);
+         res.write(err.message);
          console.log("error listing the " + dirName + " directory" + err);
          res.end();
          return;
