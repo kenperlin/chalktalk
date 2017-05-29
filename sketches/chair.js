658c8e58480a67efae2f28d47390e4e99026b8aa
Ken <ken.perlin@gmail.com>
2017-05-28 21:59:55 -0400

    Added input option to sketches/chair.js.

--- sketches/chair.js
+++ sketches/chair.js
@@ -4,8 +4,9 @@ function() {
    this.onSwipe[2] = ['stand\nup', function() { this.state = 0; }];
    this.onSwipe[6] = ['sit\ndown', function() { this.state = 1; }];
    this.render = function(elapsed) {
-      mCurve(makeSpline([ [-.4,1], [-.32, .5             ], [-.3,0] ]).concat([[-.4,-1]]));
-      mCurve(makeSpline([ [-.3,0], [ .1 ,-.1 * this.state], [ .5,0] ]).concat([[ .6,-1]]));
+      this._state = def(this.in[0]) ? this.inValue[0] : this.state;
+      mCurve(makeSpline([ [-.4,1], [-.32, .5              ], [-.3,0] ]).concat([[-.4,-1]]));
+      mCurve(makeSpline([ [-.3,0], [ .1 ,-.1 * this._state], [ .5,0] ]).concat([[ .6,-1]]));
    }
-   this.output = function() { return this.state; }
+   this.output = function() { return this._state; }
 }
