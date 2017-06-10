fec5e2f89b3c8b2d548b285f7267819c1c98a7a3
Aaron C Gaudette <aaron@holojamvr.com>
2017-06-10 16:23:34 -0400

    Refactor and lint node server

--- server/main.js
+++ server/main.js
@@ -1,202 +1,182 @@
-var bodyParser = require("body-parser");
-var express = require("express");
-var formidable = require("formidable");
-var fs = require("fs");
-var http = require("http");
-var path = require("path");
+'use strict'
+// TODO: Write tests!
 
+const parser = require('body-parser');
+const express = require('express');
+const formidable = require('formidable');
+const fs = require('fs');
+const path = require('path');
 
-var ttDgram = require('dgram');
-var ttServer = ttDgram.createSocket('udp4');
-ttServer.on('listening', function () { });
-ttServer.on('message', function (message, remote) { ttData.push(message); });
-ttServer.bind(9090, '127.0.0.1');
-ttData = [];
+const dgram = require('dgram');
+const udp = dgram.createSocket('udp4');
 
+udp.on('message', message => dgram.push(message));
+udp.bind(9090, '127.0.0.1');
 
-var app = express();
-var port = process.argv[2] || 11235;
+const app = express();
+app.use(express.static('./')); // Serve static files from main directory
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
 
-// serve static files from main directory
-app.use(express.static("./"));
+const http = require('http');
+let server = http.Server(app);
 
-// handle uploaded files
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.route("/upload").post(function(req, res, next) {
-   var form = formidable.IncomingForm();
-   form.uploadDir = "./sketches";
+const port = process.argv[2] || 11235;
+let data = [], time = 0;
+
+String.prototype.endsWith = suffix => {
+   return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
+
+String.prototype.contains = substr => {
+   return this.indexOf(substr) > -1;
+};
+
+function returnString(res, str) {
+   res.writeHead(200, { 'Content-Type': 'text/plain' });
+   res.write(str + '\n');
+   res.end();
+};
+
+function readDir(res, dirName, extension) {
+   fs.readdir('./' + dirName + '/', (err, files) => {
+      if (err) {
+         res.writeHead(500, { 'Content-Type': 'text/plain' });
+         res.write(err);
+         console.log('error listing the ' + dirName + ' directory' + err);
+         res.end();
+         return;
+      }
+
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      for (let i = 0; i < files.length; ++i) {
+         if (!extension || files[i].toLowerCase().endsWith(extension.toLowerCase())) {
+            res.write(files[i] + '\n');
+         }
+      }
+      res.end();
+   });
+}
+
+/* Handle uploaded files */
+
+app.route('/upload').post((req, res, next) => {
+   let form = formidable.IncomingForm();
+   form.uploadDir = './sketches';
    form.keepExtensions = true;
 
-   form.parse(req, function(err, fields, files) {
-      res.writeHead(200, {"content-type": "text/plain"});
+   form.parse(req, (err, fields, files) => {
+      res.writeHead(200, { 'content-type': 'text/plain' });
       res.write('received upload:\n\n');
 
-      var filename = fields.sketchName;
-      var suffix = ".js";
+      let filename = fields.sketchName;
+      let suffix = '.js';
+
       if (filename.indexOf(suffix, filename.length - suffix.length) == -1)
          filename += suffix;
 
-      fs.writeFile(form.uploadDir + "/" + filename, fields.sketchContent, function(err) {
-         if (err) {
-            console.log(err);
-         } else {
-            console.log("file written");
-         }
+      fs.writeFile(form.uploadDir + '/' + filename, fields.sketchContent, err => {
+         if (err) console.log(err);
+         else console.log('File written');
       });
 
       res.end();
    });
 });
 
-app.route("/getTT").post(function(req, res, next) {
-   var form = formidable.IncomingForm();
-   form.parse(req, function(err, fields, files) {
-      if (ttData.length > 0) {
-         returnString(res, ttData[0]);
-         ttData = [];
+app.route('/getTT').post((req, res, next) => {
+   let form = formidable.IncomingForm();
+
+   form.parse(req, (err, fields, files) => {
+      if (data.length > 0) {
+         returnString(res, data[0]);
+         data = [];
       }
    });
 });
 
-app.route("/set").post(function(req, res, next) {
-   var form = formidable.IncomingForm();
-   form.parse(req, function(err, fields, files) {
-      res.writeHead(200, {"content-type": "text/plain"});
+app.route('/set').post((req, res, next) => {
+   let form = formidable.IncomingForm();
+
+   form.parse(req, (err, fields, files) => {
+      res.writeHead(200, {'content-type': 'text/plain'});
       res.write('received upload:\n\n');
 
-      var key = fields.key;
+      let key = fields.key;
+      let suffix = '.json';
 
-      var suffix = ".json";
       if (key.indexOf(suffix, key.length - suffix.length) == -1)
          key += suffix;
 
-      fs.writeFile(key, fields.value, function(err) {
-         if (err) {
-            console.log(err);
-         } else {
-            console.log("file written");
-         }
+      fs.writeFile(key, fields.value, err => {
+         if (err) console.log(err);
+         else console.log('File written');
       });
 
       res.end();
    });
 });
 
-app.route("/writeFile").post(function(req, res, next) {
-   var form = formidable.IncomingForm();
-   form.parse(req, function(err, fields, files) {
+app.route('/writeFile').post((req, res, next) => {
+   let form = formidable.IncomingForm();
+
+   form.parse(req, (err, fields, files) => {
       fs.writeFile(fields.fileName, JSON.parse(fields.contents),
-         function(err) { if (err) console.log(err); }
+         err => { if (err) console.log(err) }
       );
       res.end();
    });
 });
 
-app.route("/talk").get(function(req, res) {
-   res.sendfile("index.html");
-});
+app.route('/talk').get((req, res) => res.sendfile('index.html'));
+app.route('/listen').get((req, res) => res.sendfile('index.html'));
 
-app.route("/listen").get(function(req, res) {
-   res.sendfile("index.html");
-});
-
-var time = 0;
-
-// handle request for the current time
-app.route("/getTime").get(function(req, res) {
+// Handle request for the current time
+app.route('/getTime').get((req, res) => {
    time = (new Date()).getTime();
    returnString(res, '' + time);
 });
 
-// handle request for list of available sketches
-app.route("/ls_sketches").get(function(req, res) {
-   readDir(res, "sketches", ".js");
-});
+// Handle request for list of available sketches
+app.route('/ls_sketches').get((req, res) => readDir(res, 'sketches', '.js'));
 
-// handle request for list of available sketch libraries
-app.route("/ls_sketchlibs").get(function(req, res) {
-   readDir(res, "sketchlibs", ".js");
-});
+// Handle request for list of available sketch libraries
+app.route('/ls_sketchlibs').get((req, res) => readDir(res, 'sketchlibs', '.js'));
 
-// handle request for list of available images
-app.route("/ls_images").get(function(req, res) {
-   readDir(res, "images");
-});
+// Handle request for list of available images
+app.route('/ls_images').get((req, res) => readDir(res, 'images'));
 
 // handle request for list of state files
-app.route("/ls_state").get(function(req, res) {
-   readDir(res, "state");
-});
+app.route('/ls_state').get((req, res) => readDir(res, 'state'));
 
-function returnString(res, str) {
-   res.writeHead(200, { "Content-Type": "text/plain" });
-   res.write(str + "\n");
-   res.end();
-};
-
-function readDir(res, dirName, extension) {
-   fs.readdir("./" + dirName + "/", function(err, files) {
-      if (err) {
-         res.writeHead(500, { "Content-Type": "text/plain" });
-         res.write(err);
-         console.log("error listing the " + dirName + " directory" + err);
-         res.end();
-         return;
-      }
-
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      for (var i = 0; i < files.length; i++) {
-         if (!extension || files[i].toLowerCase().endsWith(extension.toLowerCase())) {
-            res.write(files[i] + "\n");
-         }
-      }
-      res.end();
-   });
-}
-
-String.prototype.endsWith = function(suffix) {
-   return this.indexOf(suffix, this.length - suffix.length) !== -1;
-};
-
-String.prototype.contains = function(substr) {
-   return this.indexOf(substr) > -1;
-};
-
-// CREATE THE HTTP SERVER
-var httpserver = http.Server(app);
-
-// WEBSOCKET ENDPOINT SETUP
+// Websocket endpoint setup
 try {
-   var WebSocketServer = require("ws").Server;
-   var wss = new WebSocketServer({ port: 22346 });
-   var websockets = [];
+   let WebSocketServer = require('ws').Server;
+   let wss = new WebSocketServer({ port: 22346 });
+   let websockets = [];
 
-   wss.on("connection", function(ws) {
-
-      for (ws.index = 0 ; websockets[ws.index] ; ws.index++)
-	 ;
+   wss.on('connection', ws => {
+      for (ws.index = 0; websockets[ws.index]; ws.index++);
       websockets[ws.index] = ws;
 
-      ws.on("message", function(msg) {
-         for (var index = 0 ; index < websockets.length ; index++)
-            if (index != ws.index)
+      ws.on('message', msg => {
+         for (let i = 0; i < websockets.length; ++i) {
+            if (i != ws.index)
                websockets[index].send(msg);
+         }
       });
 
-      ws.on("close", function() {
-         // REMOVE THIS WEBSOCKET
-         websockets.splice(ws.index, 1);
-      });
+      // Remove this websocket
+      ws.on('close', () => websockets.splice(ws.index, 1));
    });
 } catch (err) {
-   console.log("\x1b[31mCouldn't load websocket library. Disabling event broadcasting."
-         + " Please run 'npm install' from Chalktalk's server directory\x1b[0m");
+   console.log(
+      '\x1b[31mCouldn\'t load websocket library. Disabling event broadcasting.'
+      + ' Run \'npm install\' from Chalktalk\'s server directory\x1b[0m'
+   );
 }
 
-// START THE HTTP SERVER
-httpserver.listen(parseInt(port, 10), function() {
-   console.log("HTTP server listening on port %d", httpserver.address().port);
-});
-
-
+// Start the http server
+server.listen(parseInt(port, 10), () =>
+   console.log('HTTP server listening on port %d', server.address().port)
+);
