2c9b2197397d27e2477ff07a6d5bb8c383e9a920
KTRosenberg <ktr254@nyu.edu>
2017-08-30 15:29:45 -0400

    avoid stash

---
+++ sketches/__growing.js
@@ -0,0 +1,164 @@
+function() {
+    this.label = 'stack';
+    this._stack = [];
+    this._openOutput = false;
+    this._removeTop = false;
+    this._pushContinuous = false;
+    this._EMPTY_OUTPUT = -1;
+    
+    this.onDrag = function(pt) {
+        console.log(time);
+    };
+    
+    this.onSwipe[0] = [
+        'peek', 
+        function() {
+           if (this._pushContinuous) {
+              return;
+           }
+           if (this._stack.length == 0) {
+              console.log("TRIED TO PEEK, BUT IS EMPTY");
+              return;
+           }
+           console.log("PEEK " + this._stack[this._stack.length - 1]);
+           this._openOutput = true;
+        }
+    ];
+
+    this.onSwipe[2] = [
+       'pop', 
+       function() {
+           if (this._pushContinuous) {
+              return;
+           }
+           if (this._stack.length == 0) {
+              console.log("TRIED TO POP, BUT IS EMPTY");
+              return;
+           } 
+           console.log("POPPED " +  this._stack[this._stack.length - 1]);
+           if (this.out[0] !== undefined) {
+             this._openOutput = true;
+             this._removeTop = true;
+           }
+           else {
+             this._stack.pop(); 
+           }
+       }
+    ];
+    this.onSwipe[6] = [
+       'push', 
+       function() {
+           if (this._pushContinuous) {
+              return;
+           }
+           if (this.inValue === undefined || this.inValue[0] === undefined /*|| this.inValue[0] == this._EMPTY_OUTPUT*/) {
+              console.log("TRIED TO PUSH, BUT NOTHING TO ADD");
+              return;
+           }
+           var input = this.inValue[0];
+           this._stack.push(input);
+           console.log("PUSHED " + input);
+       }
+    ];
+    this.onSwipe[4] = [
+       'push continuously',
+       function() {
+          if (this.inValue[0] === undefined /*|| this.inValue[0] == this._EMPTY_OUTPUT*/) {
+              this._pushContinuous = false;
+              return;
+          }
+
+          this._pushContinuous = !this._pushContinuous;
+       }
+    ];
+
+    this.onSwipe[7] = [
+      'debug',
+       function() {
+          console.log(this);
+       }
+    ];
+
+    this.render = function() {
+       mCurve([[-1, 1], [1, 1]]);
+       this.duringSketch(function() {
+          mCurve([[0, 1], [0, -.3]]);
+          mCurve([[-.2, -.1], [0, -.3], [.2, -.1]]);
+       });
+       this.afterSketch(function() {
+           // console.log(this._stack  + " " + this._stack.length);
+           var offset = 0;
+           var frameHeight = .2
+           for (var i = 0; i < this._stack.length; i++) {
+              mLine([-1, 1 - offset], [-1, 1 - offset - frameHeight]);
+              mLine([-1, 1 - offset - frameHeight], [1, 1 - offset - frameHeight]);
+              mLine([1,  1 - offset - frameHeight], [1, 1 - offset]);
+
+              offset += frameHeight;
+
+                textHeight(this.mScale(.2));
+                mText(this._stack[i], [0, -offset + 5 * frameHeight], .4, 1);
+
+              // if (offset > 15 ) {
+              //      break;
+              // }
+           }
+           if (this._stack.length > 0) {
+              mLine([-1.1, 1 - offset + (1 * frameHeight)], [-1.1, 1 - offset + (0 * frameHeight)]);
+              mLine([1.1,  1 - offset + (1 * frameHeight)], [1.1, 1 - offset + (0 * frameHeight)]);
+           }
+
+
+           mCurve([[-.2, -.1 - offset], [0, -.3 - offset], [.2, -.1 - offset]]);
+           mCurve([[0, 1 - offset], [0, -.3 - offset]]);
+
+           if (this._pushContinuous) {
+               if (this.inValue[0] === undefined) {
+                  this._pushContinuous = false;
+               }
+               else {
+                  this._stack.push(this.inValue[0]);
+              }
+          }
+       });
+    }
+
+    this.output = function() {
+      if (this._stack.length == 0) {
+        // return this._EMPTY_OUTPUT;
+        return 0;
+      }
+      if (this._removeTop) {
+          this._removeTop = false;
+          var val = this._stack.pop();
+          return val;
+      }
+      return this._stack[this._stack.length - 1];
+    };
+
+    // this.output = function() {
+    //     if (this._openOutput) {
+    //         this._openOutput = false;
+    //         if (this._removeTop) {
+    //             this._removeTop = false;
+    //             var val = this._stack.pop();
+    //             return val;
+    //         }
+    //         return this._stack[this._stack.length - 1];
+    //     }
+    // };
+
+    // this.output = function() {
+    //    this._openOutput = true;
+    //    if (this._stack.length > 0) {
+    //         if (this._removeTop) {
+    //             this._removeTop = false;
+    //             var val = this._stack.pop();
+    //             return val;
+    //         }
+    //         return this._stack[this._stack.length - 1];
+    //    }
+    //    console.log("HMMMMMMMMMMM");
+    //    return "_NONE_";
+    // };
+}
---
+++ sketches/linked_list_2.js
@@ -0,0 +1,275 @@
+function() {
+    this.label = 'LinkedList2';
+    this.is3D = true;
+
+    // CURRENTLY PARTIALLY-SUPPORTED OPERATIONS:
+    //   insertFront
+    //   removeFront
+    //   remove (removeArbitrary)
+    //   reverseInPlace
+    //   COMPACT (just for testing/fun, not high priority-wise)
+    //
+
+    this.openOutput = false;
+
+    this.onSwipe[4] = [
+       'insert front',
+       function() {
+           if (this.inValue === undefined || this.inValue[0] === undefined) {
+              return;
+           }
+           var input = this.inValue[0];
+           this.list.insertFront(input);
+       }
+    ];
+
+   this.onSwipe[0] = [
+      'remove front', 
+      function() {
+         if (this.list.size() == 0) {
+            return;
+         } 
+           //if (this.out[0] !== undefined) {
+           //  this.openOutput = true;
+           //}
+           //else {
+         this.list.removeFront(); 
+           //}
+      }
+   ];
+
+   this.onSwipe[2] = [
+      'remove arbitrary (data)',
+      function() {
+         if (this.list.size() == 0) {
+            return;
+         } 
+         //if (this.out[0] !== undefined) {
+         //   this.openOutput = true;
+         //}
+         //else {
+         if (this.inValue === undefined || this.inValue[0] === undefined) {
+            return;
+         }
+         var input = this.inValue[0];
+            this.list.removeByData(input);
+         //}
+      }
+   ];
+   this.onSwipe[3] = [
+      'remove arbitrary (selection)',
+      function() {
+         if (this.list.size() == 0) {
+            return;
+         }
+         let s = this.list.getSelectedStructure();
+         if (s === null) {
+            return;
+         }
+         this.list.removeByPtr(s.structure);
+      }
+   ];
+
+    // this.onSwipe[6] = [
+    //   'empty',
+    //   function() {
+    //     this.stack = [];
+    //   }
+    // ];
+
+   this.onSwipe[6] = [
+      "reverse in place",
+      function() {
+         this.list.reverseInPlace();
+      }
+   ];
+
+   this.onSwipe[7] = [
+      "compact",
+      function() {
+         this.list._compact();
+      }
+   ];
+
+   this.setState = function(stateType) {
+      // let result = this.list.setState(stateType);
+      // if (result == "pending") {
+      //   
+      // }
+      // else if (result == "cancel") {
+      //   
+      // }
+   }
+
+
+
+   this.setup = function() {
+      this.list = new LinkedList.SinglyLinked(this);
+
+      this.demonstrationCode = {
+         "SL_NODE" : [
+         "struct list_node<T> {",
+         "    T data;",
+         "    struct list_node * next;",
+         "};"
+         ],
+
+         // "SL_INSERT_ARBITRARY" : [
+         // "void <T> insert(T value, size_t idx)",
+         // "{",
+         // "    struct list_node * to_insert = new list_node(value);",
+         // "    struct list_node * curr = this->head;",
+         // "    for (size_t i = 0; i < idx && curr != nullptr; i++) {",
+         // "        curr = curr->next",
+         // "    }",
+         // "}"
+         // ],
+
+         "SL_REMOVE_ARBITRARY" : [
+         "void <T> remove(T value)",
+         "{",
+         "    struct list_node * prev = nullptr;",
+         "    struct list_node * curr = this->head;",
+         "",
+         "    while (curr != nullptr && curr->data != value) {",
+         "        prev = curr;",
+         "        curr = curr->next;",
+         "    }",
+         "    if (curr == nullptr) {",
+         "        return;",
+         "    }",
+         "",
+         "    if (prev == nullptr) {",
+         "        this->head = curr->next;",
+         "    } else {",
+         "        prev->next = curr->next;",
+         "    }",
+         "",
+         "    curr->next = nullptr;",
+         "    free(curr);",
+         "    this->_size--;",
+         "}"         
+         ],
+
+         "SL_REMOVE_ARBITRARY_LINUS_TORVALDS" : [
+         // https://medium.com/@bartobri/applying-the-linus-tarvolds-good-taste-coding-requirement-99749f37684a
+         // LINUS TORVALDS STYLE
+         "void <T> remove(T value)",
+         "{",
+         "    struct list_node ** indirect = &(this->head);",
+         "",
+         "    while ((*indirect) != nullptr && (*indirect)->data != value) {",
+         "        indirect = &(*indirect)->next;",
+         "    }",
+         "    if ((*indirect) == nullptr) {",
+         "        return;",
+         "    }",
+         "",
+         "    struct list_node * to_free = *indirect",
+         "    *indirect = (*indirect)->next;",
+         "",
+         "    to_free->next = nullptr;",
+         "    free(to_free);",
+         "    this->_size--;",
+         "}",
+         ],
+
+         "SL_REVERSE_IN_PLACE" : [
+         "void reverse_in_place(void)",
+         "{",
+         "    struct list_node * curr = this->head;",
+         "    struct list_node * prev = nullptr;",
+         "    while (curr != nullptr) {",
+         "        next = curr->next;",
+         "        curr->next = prev;",
+         "        prev = curr;",
+         "        curr = next;",
+         "    }",
+         "    this->head = prev;",
+         "}"
+         ],
+
+      };
+   };
+
+   this.onPress = function(p) {
+      // if (this.list.size() <= 0) {
+      //   return;
+      // }
+
+      // let bla = this.list.head.bound.pos;
+      // bla.x = p.x;
+      // bla.y = p.y;
+      // bla.z = p.z;
+      //console.log(p.x, p.y, p.z);
+      this.selectSubstructure(p);
+   };
+
+   this.onDrag = function(p) {
+      // if (this.list.size() <= 3) {
+      //   return;
+      // }
+
+      // let bla = this.list.head.bound.pos;
+      // bla.x = p.x;
+      // bla.y = p.y;
+      // bla.z = p.z;    
+   };
+
+   this.selectSubstructure = function(p) {
+      this.list.selectSubstructure(p);
+   };
+
+   // linkedlist.SinglyLinked.Operation = Object.freeze({
+   //    IDLE : '1',
+   //    INSERT_FRONT : '2',
+   //    REMOVE_FRONT : '3',
+   //    INSERT_ARBITRARY : '4',
+   //    REMOVE_ARBITRARY : '5',
+   //    REVERSE_IN_PLACE : '6',
+   // });
+
+   this.printed = false;
+   this.render = function(elapsed) {
+      this.duringSketch(function() {
+         let seventh = 1 / 7;
+         let height = 0.25;
+         mCurve([[1, 0], [-1, 0]]);
+         mCurve([
+                  [-1 + (4 * seventh) + .75, -height], [-1 + (4 * seventh) + .75, height],
+                  [-1 + seventh + .25, height],  [-1 + seventh + .25, -height],
+                  [-1 + (4 * seventh) + .75, -height]
+         ]);
+      });
+      this.afterSketch(function() {
+         this.list.print();
+         this.list.update();
+      });
+   };
+
+   this.over = function(other) {
+
+   };
+
+   this.under = function(other) {
+      if (other.output === undefined) {
+         return;
+      }
+
+      let out = other.output();
+      let additionPoint = this.list.getSelectedStructure();
+      if (additionPoint === null) {
+         this.list.insertFront(out);
+      }
+      else {
+         this.list.insertByPtr(out, additionPoint.structure);
+      }
+      other.fade();
+      other.delete();
+   };
+
+
+    this.output = function() {
+      return this.list.head;
+    };
+}
