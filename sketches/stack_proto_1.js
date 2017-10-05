d8ae4fb78a533ed786fec5ec86761eef95a1797a
Pat Sukhum <patsukhum@172-16-189-18.DYNAPOOL.NYU.EDU>
2017-10-05 15:37:04 -0400

    added stack

--- sketches/stack_proto_1.js
+++ sketches/stack_proto_1.js
@@ -1,79 +1,127 @@
 function() {
    this.label = 'Stack';
 
-   let ctx = this;
-   function Stack() {
-     this.size = 0;
-     this.array = [];
+   let sketchCtx = this;
+
+   function Stack(sketchCtx) {
+      this.sketchCtx = sketchCtx;
+      this.array = [];
 
 
-     Stack.Item = function(value, centerY, centerX=0, scale = 1){
-       this.center = [centerX, centerY];
-       this.value = value;
-       this.scale = scale;
+      Stack.Item = function(value){
+         let centerX = 0;
+         let centerY = sketchCtx.stack.size();
+         let scale = 1;
+         this.center = [centerX, centerY];
+         this.value = value;
+         this.scale = scale;
+      }
+      Stack.Item.prototype = {
+         drawItem: function() {
+            sketchCtx.drawItem(this.center, this.scale, this.value);
+        }
      }
-     Stack.Item.prototype = {
-       drawItem: function() {
-         let center = this.center;
-         let scale = this.scale;
-         let left = center[0] - scale;
-         let right = center[0] + scale;
-         let bottom = center[1] - scale/2;
-         let top = center[1] + scale/2;
-         mLine([left,bottom],[right,bottom]);
-         mLine([left,bottom],[left, top]);
-         mLine([right, bottom],[right,top]);
-         mLine([left,top],[right,top]);
-         textHeight(ctx.mScale(.4));
-         mText(this.value, center, .5, .5, .5);
-       }
-     }
-
-     this.push = function(item) {
-       this.array.push(item);
-       this.size += 1;
-     }
-
-     this.draw = function() {
-      //  console.log(this.size);
-      //  this.array.forEach(function(item){
-      //    item.drawItem();
-      //  })
-       for (i=0;i<this.array.size;i++){
-         this.array[i].drawItem();
-       }
-     }
-
-     Stack.prototype = {
-       push: function(item) {
-         this.array.push(item);
-         this.size += 1;
-       },
-       pop: function(){
-         this.size -= 1;
-         return this.array.pop();
-       },
-       draw: function(){
-         this.array.forEach(function(item){
-           item.drawItem();
-         })
-       },
-     };
    }
 
+
+   this.drawItem = function(center, scale, value) {
+      let left = center[0] - scale;
+      let right = center[0] + scale;
+      let bottom = center[1] - scale/2;
+      let top = center[1] + scale/2;
+      mLine([left,bottom],[right,bottom]);
+      mLine([left,bottom],[left, top]);
+      mLine([right, bottom],[right,top]);
+      mLine([left,top],[right,top]);
+      textHeight(this.mScale(.3));
+      mText(value, center, .5, .5, .5);
+   }
+
+
+   Stack.prototype = {
+
+      push: function(item) {
+         let stackItem = new Stack.Item(item);
+         this.array.push(stackItem);
+      },
+      pop: function(){
+         return this.array.pop();
+      },
+      draw: function(){
+         this.array.forEach(function(item){
+            item.drawItem();
+         })
+      },
+      size: function(){
+         return this.array.length;
+      },
+      emptyStack: function() {
+         while (this.array.length !== 0){
+            this.array.pop();
+         }
+      }
+   };
+
    this.setup = function() {
-     this.stack = new Stack();
+     this.stack = new Stack(this);
      for (i=0;i<5;i++){
-       this.stack.push(new Stack.Item(i+1,i));
+       this.stack.push(i+1);
      }
-     this.stack.draw();
+
+   }
+
+   this.under = function(other) {
+      if (other.output === undefined) {
+         return;
+      }
+
+      let out = other.output();
+      out = Number(1 * out);
+
+      this.stack.push(out)
+
+      other.fade();
+      other.delete();
+
+   };
+
+   this.isClickInsideStack = function(p){
+      let size = this.stack.size();
+      let scale = 1; // Temp
+      let xOffset = scale;
+      let yOffset = scale/2;
+      let left = -scale;
+      let right = scale;
+      // origin of stack is at (0,scale/2)
+      let top = size * scale - scale/2;
+      let bottom = 0 - scale/2;
+
+      return p.x > left && p.x < right && p.y < top && p.y > bottom;
+   }
+
+
+   this.onPress = function(p) {
+      if (this.isClickInsideStack(p)){
+         this.stack.pop();
+      }
    }
 
    this.render = function() {
      this.duringSketch(function() {
-       mDrawOval([-1,-1], [1,1], 32, 0, -PI);
+        mDrawOval([-1,-1], [1,1], 32, 0, -PI);
      });
      this.afterSketch(function() {
+        if (this.inValue[0] !== undefined){
+           sketchCtx.stack.emptyStack();
+           this.inValue[0].forEach(function(item){
+              sketchCtx.stack.push(item);
+           })
+           this.stack.draw();
+
+        } else {
+           this.stack.draw();
+        }
+
 
      });
    }
