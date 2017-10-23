bf2f1bfb90103a175a7d353ad72bdacaef3844cc
adellelin <adelle.lin@gmail.com>
2017-10-23 12:36:10 -0700

    added state to the hue test script

--- server/hue-bridge.js
+++ server/hue-bridge.js
@@ -5,10 +5,31 @@ var Hue = require('philips-hue');
 
 var hue = new Hue;
 var configFile = process.env.HOME+'/.philips-hue.json';
+var hueState = false;
 
 hue.bridge = "10.10.11.158";  // from hue.getBridges
 hue.username = "SEkMCLj5vQP1ZP9ihJRwKSWIxllAX9jY2J7r9NB3"; // from hue.auth
 hue.light(1).off()
+
+if (hueState == false){
+   hue.light(1).on()
+   hueState = true;
+ } else if (hueState == true){
+   hue.light(1).off()
+   hueState = false;
+ }
+
+var brightness = 120;
+var brightnessDiff = 0;
+
+for (var i = 0; i < 50; i++){
+  brightnessDiff += 1000;
+  var state = {bri: brightness, sat: 120, hue: brightnessDiff};
+   hue.light(1).setState(state).then(console.log).catch(console.error);
+}
+
+ //if (hueState)
+
 //hue.light(1).setState({effect: "colorloop"});
 
 // hue.getBridges()
