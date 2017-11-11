de31081cab9f2b1f50f5591e5732b4a9e77afcef
KTRosenberg <ktr254@nyu.edu>
2017-11-10 19:08:16 -0500

    playing with basic structure of ptr object, early

--- sketchlibs/visualPointer.js
+++ sketchlibs/visualPointer.js
@@ -3,13 +3,38 @@
 let VisualPointer = (function() {
    let _vp = {};
 
-   function VisualPointer() {
+   function VisualPointer(self, pointee) {
+      this.self = self;
+      this.pointee = pointee;
       // TODO
       console.log("CREATING VISUAL POINTER");
    }
    VisualPointer.prototype = {
+      assign : function() {
+
+      },
+      draw : function() {
+
+      }
    }
-   _vp.create = VisualPointer;
+   _vp.createPtr = VisualPointer;
+
+   function VisualEdge(a, b, isDirected = true) {
+      this.a = a;
+      this.b = b;
+      this.isDirected = isDirected;
+      // TODO
+      console.log("CREATING VISUAL EDGE");
+   }
+   VisualEdge.prototype = {
+      assign : function() {
+
+      },
+      draw : function() {
+
+      }
+   }
+   _vp.createEdge = VisualEdge;
 
    // TODO
 
