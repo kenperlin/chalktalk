78659a97a53a3a18387871ed5bb90a2cdd316657
Ken Perlin <ken@172-16-22-216.DYNAPOOL.NYU.EDU>
2017-12-23 16:33:25 -0500

    In sketches/axes.js changed fadedRGB() to fadedColor().

--- sketches/axes.js
+++ sketches/axes.js
@@ -98,7 +98,7 @@ function() {
                m.scale(.5,.5,.5);
                if (inValue && inValue.fillMode > 0) {
                   if (inValue.fillMode == 1) {
-                     color(fadedRGB(0.25, parseRGBA(c = _g.strokeStyle)));
+                     color(fadedColor(0.25, parseRGBA(c = _g.strokeStyle)));
                      m.translate(0,0,-.001);
                      mFillCurve(inValue);
                      m.translate(0,0,.001);
