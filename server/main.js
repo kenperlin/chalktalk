775a1d60c4dec404ce5beb147a3303fe64d99844
Zhenyi He <snowymohehe@gmail.com>
2018-04-04 22:41:41 -0400

    add support for listening to udp, to receive 3dof from android simulator

--- server/main.js
+++ server/main.js
@@ -6,11 +6,12 @@ const express = require('express');
 const formidable = require('formidable');
 const fs = require('fs');
 const path = require('path');
+const dgram = require('dgram');
 
 // behave as a relay
-//const holojam = require('holojam-node')(['relay']);
+const holojam = require('holojam-node')(['relay']);
 // behave as a receiver and sender
-const holojam = require('holojam-node')(['emitter', 'sink']);
+//const holojam = req uire('holojam-node')(['emitter', 'sink'], '192.168.1.70');
 
 const app = express();
 app.use(express.static('./')); // Serve static files from main directory
@@ -24,6 +25,61 @@ let server = http.Server(app);
 server.listen(parseInt(port, 10), () =>
    console.log('HTTP server listening on port %d', server.address().port)
 );
+
+// listen to android simulation through udp
+const udpServer = dgram.createSocket('udp4');
+
+udpServer.on('error', (err) => {
+  console.log(`udpServer error:\n${err.stack}`);
+  udpServer.close();
+});
+
+var PORT = 11000;
+var HOST = '192.168.1.70';
+
+udpServer.on('listening', function () {
+    var address = udpServer.address();
+    console.log('UDP udpServer listening on ' + address.address + ":" + address.port);
+});
+
+//
+// Summary:
+//   Began = 1,  A finger touched the screen.
+//   Moved = 2,   A finger moved on the screen.
+//   Stationary = 3,  A finger is touching the screen but hasn't moved.
+//  Ended = 4,   A finger was lifted from the screen. This is the final phase of a touch.
+//  Canceled = 5   The system cancelled tracking for the touch.
+var curDaydreamInput = {
+	rx:0,
+	ry:0,
+	rz:0,
+	state:0,
+	x:0,
+	y:0
+};
+
+udpServer.on('message', function (message, remote) {
+    //console.log(remote.address + ':' + remote.port +' - ' + message + message.length);
+	if(message.length != 24)
+		return;
+	var index = 0;
+    curDaydreamInput.rx = message.readFloatLE(index);
+	index += 4;
+	curDaydreamInput.ry = message.readFloatLE(index);
+	index += 4;
+	curDaydreamInput.rz = message.readFloatLE(index);
+	index += 4;
+	curDaydreamInput.state = message.readInt32LE(index);
+	index += 4;
+	curDaydreamInput.x = message.readFloatLE(index);
+	index += 4;
+	curDaydreamInput.y = message.readFloatLE(index);
+	//console.log(rx,ry,rz,state,x,y);
+	console.log(curDaydreamInput);
+});
+
+udpServer.bind(PORT, HOST);
+// end of udp server
 
 let data = [], time = 0;
 
